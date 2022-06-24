61fa8397e46f6be8bd1460560d36d0ac401537e2
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-06-24 13:25:54 +0100

    working on subtopbar

--- src/Layout/Components/SubTopBar.js
+++ src/Layout/Components/SubTopBar.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import Drawer from "@mui/material/Drawer";
+import SideBar from "./SideBar";
 
 const SubTopBar = () => {
   const [hideOrShow, setHideOrShow] = useState(false);
   const [moduleDrawer, setModuleDrawer] = useState({
+    left: false,
+  });
+
+  const [featuresDrawer, setFeaturesDrawer] = useState({
     left: false,
   });
 
@@ -21,32 +26,47 @@ const SubTopBar = () => {
     setModuleDrawer({ ...moduleDrawer, [anchor]: open });
   };
 
+  const toggleDrawerFeatures = (anchor, open) => () => {
+    setFeaturesDrawer({ ...moduleDrawer, [anchor]: open });
+  };
+
   return (
     <>
+      {/* Modules mobile drawer */}
       <Drawer open={moduleDrawer["left"]} onClose={toggleDrawer("left", false)}>
         <div className="flex items-center h-screen overflow-auto">
           <div className="Container flex gap-4 flex-col text-accent font-medium text-sm">
-            <div className="flex items-center gap-1 cursor-pointer">
+            <div className="module_list_item_mobile">
               <i className="ri-scales-line text-base"></i>
               <span>Performance</span>
             </div>
-            <div className="flex items-center gap-1 cursor-pointer">
+            <div className="module_list_item_mobile">
               <i className="ri-money-dollar-circle-line text-base"></i>
               <span>Payroll</span>
             </div>
-            <div className="flex items-center gap-1 cursor-pointer">
+            <div className="module_list_item_mobile">
               <i className="ri-team-line text-base"></i>
               <span>Manage employee</span>
             </div>
-            <div className="flex items-center gap-1 cursor-pointer">
+            <div className="module_list_item_mobile">
               <i className="ri-user-shared-2-line text-base"></i>
               <span>Recruitment</span>
             </div>
-            <div className="flex items-center gap-1 cursor-pointer">
+            <div className="module_list_item_mobile">
               <i className="ri-honour-line text-base"></i>
               <span>Attendance</span>
             </div>
           </div>
+        </div>
+      </Drawer>
+
+      {/* Features drawer mobile */}
+      <Drawer
+        open={featuresDrawer["left"]}
+        onClose={toggleDrawerFeatures("left", false)}
+      >
+        <div>
+          <SideBar/>
         </div>
       </Drawer>
       <div
@@ -63,26 +83,29 @@ const SubTopBar = () => {
           >
             Modules
           </span>
-          <i className="ri-menu-line text-accent text-xl cursor-pointer"></i>
+          <i
+            className="ri-menu-line text-accent text-xl cursor-pointer"
+            onClick={toggleDrawerFeatures("left", true)}
+          ></i>
         </div>
         <div className="Container hidden lg:flex  items-center md:flex-row flex-col gap-4 text-accent font-medium text-sm">
-          <div className="model_list_item">
+          <div className="module_list_item">
             <i className="ri-scales-line text-base"></i>
             <span>Performance</span>
           </div>
-          <div className="model_list_item">
+          <div className="module_list_item">
             <i className="ri-money-dollar-circle-line text-base"></i>
             <span>Payroll</span>
           </div>
-          <div className="model_list_item">
+          <div className="module_list_item">
             <i className="ri-team-line text-base"></i>
             <span>Manage employee</span>
           </div>
-          <div className="model_list_item">
+          <div className="module_list_item">
             <i className="ri-user-shared-2-line text-base"></i>
             <span>Recruitment</span>
           </div>
-          <div className="model_list_item">
+          <div className="module_list_item">
             <i className="ri-honour-line text-base"></i>
             <span>Attendance</span>
           </div>
