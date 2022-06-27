7a861c025a3c73b35813918a15795cbfaf9fc220
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-06-27 12:54:48 +0100

    authroutes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import Login from "./Auth/Pages/Login";
-import Register from "./Auth/Pages/Register";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ResetPassword from "./Auth/Pages/ResetPassword";
-import Home from "./Home/Pages/Home";
-import Domains from "./Settings/Pages/Domains";
+import AuthRoutes from "./Auth/Routes/AuthRoutes";
 
 function App() {
   return (
     <Router>
+      <AuthRoutes />
+      
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-
-        <Route path="*">{/* <span>Not Found</span> */}</Route>
+      <Route path="*" element={<span>Not found1</span> }/>
       </Routes>
     </Router>
   );
