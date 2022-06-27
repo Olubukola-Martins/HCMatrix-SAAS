7a5e179efb27c0829832916ec08aedd755b5f0f0
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-06-27 12:21:27 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-
 import Login from "./Auth/Pages/Login";
 import Register from "./Auth/Pages/Register";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ResetPassword from "./Auth/Pages/ResetPassword";
 import Home from "./Home/Pages/Home";
 
 function App() {
   return (
     <Router>
-      <Route path="/" exact>
-        <Home/>
-      </Route>
-     <Route path="/login">
-        <Login/>
-      </Route>
-      <Route path="/register">
-        <Register/>
-      </Route>
-      <Route path="/reset-password">
-        <ResetPassword/>
-      </Route>
-      <Route path="*">
-        <span>Not Found</span>
-      </Route>
-    </Router>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
 
+      {/* <Home/>
+      </Route> */}
+      {/* <Route path="/login">
+        <Login/>
+      </Route> */}
+      {/* <Route path="/register">
+        <Register/>
+      </Route> */}
+      {/* <Route path="/reset-password">
+        <ResetPassword/>
+      </Route> */}
+
+      {/* <Route path="*">
+        <span>Not Found</span>
+      </Route> */}
+            </Routes>
+    </Router>
   );
 }
 
