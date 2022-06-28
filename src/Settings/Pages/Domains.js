4ff2b292e1dea0e8a2d87c8999ecc8262f672f64
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-06-28 14:53:13 +0100

    themes

--- src/Settings/Pages/Domains.js
+++ src/Settings/Pages/Domains.js
@@ -1,23 +1,8 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import Themes from "../../Themes/Themes";
 
 import DashboardLayout from "../../Layout/DashboardLayout";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  maxwidth: 400,
-  bgcolor: "background.paper",
-  pt: 2,
-  pb: 4,
-  pr: 4,
-  pl: 4,
-  borderRadius: 4,
-};
 
 const Domains = () => {
   const [open, setOpen] = React.useState(false);
@@ -25,7 +10,7 @@ const Domains = () => {
   const handleClose = () => setOpen(false);
   return (
     <DashboardLayout>
-      <div className="Container pb-36">
+      <div className="Container pb-36 pt-5">
         <div className="flex items-center justify-between">
           <h4 className="text-accent text-base">
             Create and Manage Domain information of your organization
@@ -56,28 +41,35 @@ const Domains = () => {
 
         {/* Domain modal */}
         <Modal open={open} onClose={handleClose}>
-          <Box sx={style}>
+          <Themes>
+            <div className="CModal" style={{ maxWidth: 400 }}>
               <span className="flex justify-end mb-2">
-              <i className="ri-close-line cursor-pointer" onClick={handleClose}></i>
+                <i
+                  className="ri-close-line cursor-pointer"
+                  onClick={handleClose}
+                ></i>
               </span>
-            <h1 className="font-bold text-lg text-accent text-center">
-              Add Domain Name
-            </h1>
-            <form className="my-4">
-              <input
-                type="text"
-                className="border py-2 pl-2 text-sm rounded w-full focus:outline-none"
-                placeholder="Domain name"
-              />
-              <span className="text-xs">Example: johndoe.com</span>
-            </form>
-            <div className="flex items-center justify-center gap-4">
-              <button className="button">Add</button>
-              <button className="transparentButton" onClick={handleClose}>
-                Cancel
-              </button>
+              <h1 className="font-bold text-lg text-accent text-center">
+                Add Domain Name
+              </h1>
+              <form className="my-4">
+                <input
+                  type="text"
+                  className="border border-slate-400 text-accent bg-transparent py-2 pl-2 text-sm rounded w-full focus:outline-none"
+                  placeholder="Domain name"
+                />
+                <span className="text-xs text-accent">
+                  Example: johndoe.com
+                </span>
+              </form>
+              <div className="flex items-center justify-center gap-4">
+                <button className="button">Add</button>
+                <button className="transparentButton" onClick={handleClose}>
+                  Cancel
+                </button>
+              </div>
             </div>
-          </Box>
+          </Themes>
         </Modal>
       </div>
     </DashboardLayout>
