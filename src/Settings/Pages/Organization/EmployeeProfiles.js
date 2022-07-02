eb8921dd22f3ba294b5d4a978de5cca0d269392f
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-07-02 23:40:33 +0100

    working on employee profile

--- src/Settings/Pages/Organization/EmployeeProfiles.js
+++ src/Settings/Pages/Organization/EmployeeProfiles.js
@@ -27,11 +27,8 @@ const EmployeeProfiles = () => {
               <h5 className="flex items-center justify-between py-2 border-b">
                 <span>Confirmed</span> <span>10</span>
               </h5>
-              <h5 className="flex items-center justify-between border-b">
+              <h5 className="flex items-center justify-between">
                 <span>Disengaged</span> <span>8</span>
-              </h5>
-              <h5 className="flex items-center justify-between py-2 border-b">
-                <span>Master records</span> <span>24</span>
               </h5>
             </div>
           </Themes>
@@ -40,7 +37,9 @@ const EmployeeProfiles = () => {
       <div className="Container pb-10">
         <div className="flex justify-between mt-5">
           <div className="flex items-center gap-3">
-            <h4 className="cursor-pointer text-accent">Employee Status</h4>
+            <h4 className="cursor-pointer text-accent font-medium">
+              Employee Status
+            </h4>
             <button
               className="flex items-center gap-2 transition duration-300 ease-in-out hover:border-slate-400 text-slate-400 border rounded text-sm px-2"
               onClick={handleClick}
@@ -58,11 +57,68 @@ const EmployeeProfiles = () => {
               Import Employee Profile
             </button>
             <i className="ri-filter-line text-xl cursor-pointer"></i>
-            <i className="ri-upload-2-line text-xl cursor-pointer"></i>
+            {/* <i className="ri-upload-2-line text-xl cursor-pointer"></i> */}
             <i
               className="ri-question-fill text-xl cursor-pointer text-gray-400"
               title="Employee profile"
             ></i>
+          </div>
+        </div>
+        <br />
+        <div className="bg-card flex items-center justify-between p-3">
+          <h5 className="font-medium">Employee Information</h5>
+          <div className="flex items-center gap-5">
+            <button className="flex items-center gap-2 text-sm text-white bg-green-600 px-2 py-1">
+              <span>Enroll Biometric</span>
+              <i className="ri-fingerprint-fill text-base"></i>
+            </button>
+            <button className="flex items-center gap-2 text-sm text-white bg-caramel px-2 py-1">
+              <span>Remove Biometric</span>
+              <i className="ri-fingerprint-fill text-base"></i>
+            </button>
+            <button className="flex items-center gap-2 text-sm text-white bg-green-600 px-2 py-1">
+              <span>Current View</span>
+              <i className="ri-file-excel-line text-base"></i>
+            </button>
+            <button className="flex items-center gap-2 text-sm text-white bg-green-600 px-2 py-1">
+              <span>All</span>
+              <i className="ri-file-excel-line text-base"></i>
+            </button>
+            <button className="flex items-center gap-2 text-sm text-white bg-caramel px-2 py-1">
+              <span>Export</span>
+              <i className="ri-file-hwp-line text-base"></i>
+            </button>
+          </div>
+
+          <div />
+        </div>
+
+        <br />
+        <div className="bg-card">
+          <div className="flex justify-end">
+            <span className="pr-3 pb-2 cursor-pointer">x</span>
+          </div>
+          <div className="px-3 pb-4 text-caramel font-semibold text-sm flex items-center justify-between">
+            <button className="border-b-2 border-caramel">
+              Enroll to E-Learning
+            </button>
+            <button className="border-b-2 border-caramel">
+              + Add to Group
+            </button>
+            <button className="border-b-2 border-caramel">
+              Assign to Role
+            </button>
+            <button className="border-b-2 border-caramel">
+              Assign to Line Manager
+            </button>
+
+            <button className="border-b-2 border-caramel">
+              Change Employee Status
+            </button>
+            <button className="border-b-2 border-caramel">
+              Change Login Status
+            </button>
+            <button className="border-b-2 border-caramel">Others</button>
           </div>
         </div>
         <br />
