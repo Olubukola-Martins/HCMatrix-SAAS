b10210c6a28229175548c98e6dcca286348e2e56
kaasikodes <74901471+kaasikodes@users.noreply.github.com>
2022-07-06 13:52:10 +0100

    redesigned settings-page

--- src/Settings/Pages/General/GeneralSettings.js
+++ src/Settings/Pages/General/GeneralSettings.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import DashboardLayout from "../../../Layout/DashboardLayout";
 import SettingNavItem from "../../Components/SettingNavItem";
 import { settingNavItems } from "../../Data";
+import FramerAccordian from "./custom/FramerAccordian";
 
 const GeneralSettings = () => {
   const [navItems, setNavItems] = useState(settingNavItems);
+
   const handleSearch = (e) => {
     const val = e.target.value;
     console.log("val =>", val);
@@ -27,11 +29,11 @@ const GeneralSettings = () => {
   };
   return (
     <DashboardLayout>
-      <div className="Container pb-20 mt-10">
-        <h4 className="font-bold text-accent text-2xl">All Settings</h4>
-        <div className="flex flex-col mt-2">
+      <div className="Container pb-96 ">
+        <h4 className="font-bold text-accent text-2xl mt-6">All Settings</h4>
+        <div className="flex flex-col mt-8">
           {/* search */}
-          <div className="flex justify-between border-0 border-b w-3/4 md:w-2/4 mt-16 self-center px-2">
+          <div className="flex justify-between border-0 border-b w-3/4 md:w-2/4 self-center px-2">
             <input
               className="border-none mb-2 flex-1 outline-0 outline-none bg-transparent"
               onChange={handleSearch}
@@ -40,28 +42,85 @@ const GeneralSettings = () => {
             <i class="fas fa-search"></i>
           </div>
           {/* Page NavItems */}
-          {navItems && navItems.length > 0 ? (
-            <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 justify-between mt-32">
-              {/* item */}
-              {navItems &&
-                navItems.map((item) => (
-                  <SettingNavItem item={item} key={item.title} />
-                ))}
-              {/* <div className='setting-nav-item mb-24'>
-                    <h5 className="font-semibold text-accent text-xl">
-                        {`Product Links`}
-                    </h5>
-                   
-                </div> */}
-            </div>
-          ) : (
-            <div className="mt-32 w-full">
-              <p className="text-accent text-2xl text-center">
-                {" "}
-                No items found
-              </p>
-            </div>
-          )}
+          <div className="mt-12 grid grid-cols-1  gap-4">
+            {/* grouped settings */}
+            {/* Basic */}
+            <FramerAccordian heading={"Basic Settings"}>
+              {navItems && navItems.length > 0 ? (
+                <div className="grid grid-cols-2 lg:grid-cols-5 gap-4">
+                  {/* item */}
+                  {navItems &&
+                    navItems.map((item) => (
+                      <SettingNavItem item={item} key={item.title} />
+                    ))}
+                </div>
+              ) : (
+                <div className="mt-32 w-full">
+                  <p className="text-accent text-2xl text-center">
+                    {" "}
+                    No items found
+                  </p>
+                </div>
+              )}
+            </FramerAccordian>
+            {/* Self service*/}
+            <FramerAccordian heading={"Self Service"}>
+              {navItems && navItems.length > 0 ? (
+                <div className="grid grid-cols-2 lg:grid-cols-5 gap-4">
+                  {/* item */}
+                  {navItems &&
+                    navItems.map((item) => (
+                      <SettingNavItem item={item} key={item.title} />
+                    ))}
+                </div>
+              ) : (
+                <div className="mt-32 w-full">
+                  <p className="text-accent text-2xl text-center">
+                    {" "}
+                    No items found
+                  </p>
+                </div>
+              )}
+            </FramerAccordian>
+            {/* Payroll */}
+            <FramerAccordian heading={"Payroll"}>
+              {navItems && navItems.length > 0 ? (
+                <div className="grid grid-cols-2 lg:grid-cols-5 gap-4">
+                  {/* item */}
+                  {navItems &&
+                    navItems.map((item) => (
+                      <SettingNavItem item={item} key={item.title} />
+                    ))}
+                </div>
+              ) : (
+                <div className="mt-32 w-full">
+                  <p className="text-accent text-2xl text-center">
+                    {" "}
+                    No items found
+                  </p>
+                </div>
+              )}
+            </FramerAccordian>
+            {/* Performance */}
+            <FramerAccordian heading={"Performance"}>
+              {navItems && navItems.length > 0 ? (
+                <div className="grid grid-cols-2 lg:grid-cols-5 gap-4">
+                  {/* item */}
+                  {navItems &&
+                    navItems.map((item) => (
+                      <SettingNavItem item={item} key={item.title} />
+                    ))}
+                </div>
+              ) : (
+                <div className="mt-32 w-full">
+                  <p className="text-accent text-2xl text-center">
+                    {" "}
+                    No items found
+                  </p>
+                </div>
+              )}
+            </FramerAccordian>
+          </div>
         </div>
       </div>
     </DashboardLayout>
---
+++ src/Settings/Pages/General/custom/FramerAccordian.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const FramerAccordian = ({ heading, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <motion.div
+      className="basic-setting border rounded overflow-hidden"
+      animate={{
+        height: isOpen ? "max-content" : "55px",
+        transition: {
+          duration: 0.5,
+        },
+      }}
+    >
+      <motion.div
+        className="heading px-6 py-4 border-0 border-b flex justify-between"
+        style={{ height: "55px" }}
+      >
+        <h5 className="text-caramel text-xl font-semibold">{heading}</h5>
+        <motion.div
+          className="dropdown-btn"
+          animate={{ rotate: isOpen ? 180 : 0 }}
+        >
+          <i
+            className="fa fa-caret-down cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          ></i>
+        </motion.div>
+      </motion.div>
+      <AnimatePresence>
+        {<motion.div className="content p-6">{children}</motion.div>}
+      </AnimatePresence>
+    </motion.div>
+  );
+};
+
+export default FramerAccordian;
