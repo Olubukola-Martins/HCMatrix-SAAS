e03c3e35ce3cf00cce74725cb054c6b6d380f3c8
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-07-14 19:59:02 +0100

    form

--- src/Settings/Components/EmployeeProfile/AddDependant.js
+++ src/Settings/Components/EmployeeProfile/AddDependant.js
@@ -2,13 +2,25 @@ import React from 'react'
 import Modal from "@mui/material/Modal";
 import Themes from "../../../Themes/Themes";
 
-const AddDependant = () => {
+const AddDependant = ({ open, handleClose }) => {
   return (
     <>
-      <Modal>
+      <Modal open={open} onClose={handleClose}>
           <Themes>
               <div className="CModal" style={{ maxWidth: 400 }}>
-                
+              <div className="flex items-center justify-between w-full mb-5">
+              <h5 className="text-base font-semibold">Add dependant</h5>
+              <i
+                class="fas fa-times cursor-pointer text-xl"
+                onClick={handleClose}
+              ></i>
+            </div>
+
+                <form>
+                  <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                    
+                  </div>
+                </form>
               </div>
           </Themes>
       </Modal>
