b8f1e7d73c8e76349656d5b90086d334890fb38d
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-07-14 20:51:44 +0100

    form

--- src/Settings/Components/EmployeeProfile/Skills.js
+++ src/Settings/Components/EmployeeProfile/Skills.js
@@ -1,33 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
 import "../../style/settingsStyle.css";
+import AddSkill from "./AddSkill";
 
 const Skills = () => {
+  const [newSkillFormModal, setNewSkillFormModal] = useState(false);
   return (
- <>
-  <div className="flex items-center justify-between">
-        <button className="button">Add Skills</button>
+    <>
+      <div className="flex items-center justify-between">
+        <button className="button" onClick={() => setNewSkillFormModal(true)}>
+          Add Skills
+        </button>
 
         <input
           type="text"
           placeholder="Search.."
           className="rounded-3xl px-4 py-1 focus:outline-none bg-mainBg placeholder:text-sm"
         />
+
+        <AddSkill
+          open={newSkillFormModal}
+          handleClose={() => setNewSkillFormModal(false)}
+        />
       </div>
 
       <table className="employee_info_table mt-5">
-          <thead>
-            <tr>
-              <th>Skill</th>
-              <th>Competency</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-        </table>
-        <div className="border text-center py-1 text-sm mt-1">
-          No Matching Records
-        </div>
- </>
-  )
-}
+        <thead>
+          <tr>
+            <th>Skill</th>
+            <th>Competency</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+      </table>
+      <div className="border text-center py-1 text-sm mt-1">
+        No Matching Records
+      </div>
+    </>
+  );
+};
 
-export default Skills
+export default Skills;
