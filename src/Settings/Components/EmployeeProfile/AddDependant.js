5653450238c4d00b091edd36c2d06cd8b891c567
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-07-14 19:46:11 +0100

    form

---
+++ src/Settings/Components/EmployeeProfile/AddDependant.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Modal from "@mui/material/Modal";
+import Themes from "../../../Themes/Themes";
+
+const AddDependant = () => {
+  return (
+    <>
+      <Modal>
+          <Themes>
+              <div className="CModal" style={{ maxWidth: 400 }}>
+                
+              </div>
+          </Themes>
+      </Modal>
+    </>
+  )
+}
+
+export default AddDependant
--- src/Settings/Components/EmployeeProfile/AddQualification.js
+++ src/Settings/Components/EmployeeProfile/AddQualification.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 import Modal from "@mui/material/Modal";
 import Themes from "../../../Themes/Themes";
 
 const AddQualification = ({ open, handleClose }) => {
   return (
     <>
-    <Modal open={open} onClose={handleClose}>
+      <Modal open={open} onClose={handleClose}>
         <Themes>
-            <div className="CModal" style={{ maxWidth: 400 }}>
+          <div className="CModal" style={{ maxWidth: 500 }}>
             <div className="flex items-center justify-between w-full mb-5">
               <h5 className="text-base font-semibold">Add Qualification</h5>
               <i
@@ -15,19 +15,65 @@ const AddQualification = ({ open, handleClose }) => {
                 onClick={handleClose}
               ></i>
             </div>
-             <form>
-                <div className='grid grid-cols-2 gap-5'>
+            <form>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
                 <div className="whiteBg_form">
-                <label>Salary</label>
-              
-              </div>
+                  <label>Qualification</label>
+                  <select name="" id="">
+                    <option value="bachelor">Bachelor</option>
+                    <option value="phd">PHD</option>
+                  </select>
                 </div>
-             </form>
-            </div>
-        </Themes>
-    </Modal>
-    </>
-  )
-}
+                <div className="whiteBg_form">
+                  <label>Title</label>
+                  <input type="text" placeholder="Qualification Title" />
+                </div>
 
-export default AddQualification
+                <div className="whiteBg_form">
+                  <label>Institution</label>
+                  <input type="text" placeholder="Institution" />
+                </div>
+                <div className="whiteBg_form">
+                  <label>Year</label>
+                  <input
+                    type="text"
+                    placeholder="03 - 05 - 2021"
+                    onFocus={(e) => (e.target.type = "date")}
+                    onBlur={(e) => (e.target.type = "text")}
+                  />
+                </div>
+                <div className="whiteBg_form">
+                  <label>Course</label>
+                  <input type="text" placeholder="User experience design" />
+                </div>
+
+                <div className="whiteBg_form">
+                  <label>Grade</label>
+                  <select name="" id="">
+                    <option value="grade_1">Grade A</option>
+                    <option value="grade_2">Grade B</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="flex items-center justify-around mt-5">
+                <button
+                  type="button"
+                  onClick={handleClose}
+                  className="transparentButton"
+                >
+                  Cancel
+                </button>
+                <button type="submit" className="button">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </Themes>
+      </Modal>
+    </>
+  );
+};
+
+export default AddQualification;
