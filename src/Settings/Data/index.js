1d6252e25883ca4173ccb545edc8d4de522e668d
kaasikodes <74901471+kaasikodes@users.noreply.github.com>
2022-07-15 15:15:57 +0100

    removed duplicate setting items

--- src/Settings/Data/index.js
+++ src/Settings/Data/index.js
@@ -1,63 +1,4 @@
 export const settingNavItems = [
-    {
-        title: 'General',
-        items: [
-            {name: 'Company', link: '/settings/company-details'},
-            {name: 'Domain', link: '/settings/domains'},
-            {name: 'Rebranding', link: '/settings/rebranding'},
-            {name: 'From Addresses', link: '/settings/from-addresses'},
-
-        ]
-    },
-    {
-        title: 'Organization',
-        items: [
-            {name: 'Users', link: '/settings/user-profile'},
-            {name: 'Employee profiles', link: '/settings/employee-profile'},
-            {name: 'Departments', link: '/settings/rebranding'},
-            {name: 'Designations', link: '/settings/from-addresses'},
-            {name: 'Locations', link: '/settings/locations'},
-            {name: 'Groups', link: '/settings/from-addresses'},
-            {name: 'Delegation', link: '/settings/from-addresses'},
-
-        ]
-    },
-    {
-        title: 'User Access Control',
-        items: [
-            {name: 'Roles', link: '/settings/company-details'},
-            {name: 'Permissions', link: '/settings/domain'},
-            {name: 'Allowed IP\'s', link: '/settings/rebranding'},
-            {name: 'Geo Restrictions', link: '/settings/from-addresses'},
-          
-
-        ]
-    },
-
-    {
-        title: 'Automation',
-        items: [
-            {name: 'Workflows', link: '/settings/company-details'},
-            {name: 'Actions', link: '/settings/domain'},
-            {name: 'Approvals', link: '/settings/rebranding'},
-            {name: 'Scheduler', link: '/settings/from-addresses'},
-          
-
-        ]
-    },
-    {
-        title: 'Data Administration',
-        items: [
-            {name: 'Import History', link: '/settings/company-details'},
-            {name: 'Export History', link: '/settings/domain'},
-            {name: 'Activity Log', link: '/settings/rebranding'},
-            {name: 'Files Usage', link: '/settings/from-addresses'},
-            {name: 'API Usage', link: '/settings/from-addresses'},
-          
-
-        ]
-    },
-
   {
     title: "General",
     items: [
@@ -156,4 +97,3 @@ export const countries = [
     label: "Isreal",
   },
 ];
-
