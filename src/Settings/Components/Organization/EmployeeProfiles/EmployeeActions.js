9fa4fb94af142d6a1741272ec66cf7476d719c35
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-07-18 12:22:54 +0100

    employee

--- src/Settings/Components/Organization/EmployeeProfiles/EmployeeActions.js
+++ src/Settings/Components/Organization/EmployeeProfiles/EmployeeActions.js
@@ -22,10 +22,8 @@ const EmployeeActions = () => {
   return (
     <div className="Container">
       <div className="bg-card mb-2">
-        <div className="flex justify-end">
-          <span className="pr-3 pb-2 cursor-pointer">x</span>
-        </div>
-        <div className="px-3 pb-4 text-caramel font-semibold text-sm grid grid-cols-5 gap-x-8 gap-y-4 flex-wrap">
+        
+        <div className="px-3 py-4 text-caramel font-semibold text-sm grid grid-cols-5 gap-x-8 gap-y-4 flex-wrap">
           <button className="rounded border py-1 border-caramel">
             Enroll to E-Learning
           </button>
