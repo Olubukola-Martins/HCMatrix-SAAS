87cf580c8c32788da757cea0c264be0fcd4ec5d2
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-07-20 12:28:09 +0100

    quick helk

--- src/Layout/Components/GlobalSupport.js
+++ src/Layout/Components/GlobalSupport.js
@@ -7,9 +7,9 @@ import chat from "../Images/chatIcon.svg";
 import query from "../Images/queryIcon.svg";
 import Themes from "../../Themes/Themes";
 
-
 const GlobalSupport = () => {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [queryModal, setQueryModal] = useState(false);
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -35,7 +35,7 @@ const GlobalSupport = () => {
         }}
       >
         <Themes>
-          <div className="rounded">
+          <div className="rounded-md">
             <div className="flex items-center gap-x-10 bg-caramel font-medium text-white px-3 py-2">
               <span>HCMatrix Quick Links</span>
               <i
@@ -59,7 +59,7 @@ const GlobalSupport = () => {
                 <li className="flex items-center gap-x-5 border-b-2 pb-1 px-3 cursor-pointer">
                   <img src={chat} alt="help video" /> <span>Chat</span>
                 </li>
-                <li className="flex items-center gap-x-5 pb-1 px-3 cursor-pointer">
+                <li onClick={() => setQueryModal(true)} className="flex items-center gap-x-5 pb-1 px-3 cursor-pointer">
                   <img src={query} alt="help video" />
                   <span>Drop Query</span>
                 </li>
@@ -81,8 +81,18 @@ const GlobalSupport = () => {
       </Tooltip>
 
       {/* query modal */}
-      <Modal>
-
+      <Modal open={queryModal} onClose={() => setQueryModal(false)}>
+        <Themes>
+          <div className="CModal" style={{ maxWidth: 500 }}>
+            <div className="flex items-center justify-between w-full mb-5">
+              <h5 className="text-base font-semibold">Drop Query</h5>
+              <i
+                class="fas fa-times cursor-pointer text-xl"
+                onClick={() => setQueryModal(false)}
+              ></i>
+            </div>
+          </div>
+        </Themes>
       </Modal>
     </>
   );
