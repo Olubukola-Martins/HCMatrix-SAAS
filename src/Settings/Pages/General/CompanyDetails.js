bbc049b5764ab3dd30b8e5d7d3ad3d7484522432
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-07-28 12:30:46 +0100

    form validate

--- src/Settings/Pages/General/CompanyDetails.js
+++ src/Settings/Pages/General/CompanyDetails.js
@@ -1,41 +1,35 @@
-
-import DashboardLayout from '../../../Layout/DashboardLayout'
+import DashboardLayout from "../../../Layout/DashboardLayout";
 // import Logo from '../Assets/round_company_logo.png'
-import CompanyInfoAccordion from '../../Components/CompanyInfoAccordion'
-import CompanySettingsAccordion from '../../Components/CompanySettingsAccordion'
-import Avatar from '@mui/material/Avatar';
-import {Link} from 'react-router-dom'
-
-
-
-
-
-
+import CompanyInfoAccordion from "../../Components/CompanyInfoAccordion";
+import CompanySettingsAccordion from "../../Components/CompanySettingsAccordion";
+import Avatar from "@mui/material/Avatar";
+import { Link } from "react-router-dom";
 
 const CompanyDetails = () => {
-
   return (
     <DashboardLayout>
-        <div className="Container pb-20 mt-10 mb-72">
-        <Link to = '/settings'>
-              <i className="fa fa-arrow-left text-accent text-lg" aria-hidden="true"></i></Link>
-            {/* img container */}
-            <div className='img-container flex justify-center mb-20'>
-                {/* <img src={Logo} alt= 'logo' className="h-28"/> */}
-                <Avatar alt="Remy Sharp" src='' />
-            </div>
-           <div className='accordions grid grid-cols-1 lg:grid-cols-1 gap-12'>
-             {/* accordian 1 */}
-             <CompanyInfoAccordion/>
-            
-
-            {/* accordian 2 */}
-            <CompanySettingsAccordion/>
-           </div>
-       
+      <div className="Container pb-20 mt-10 mb-72">
+        <Link to="/settings">
+          <i
+            className="fa fa-arrow-left text-accent text-lg"
+            aria-hidden="true"
+          ></i>
+        </Link>
+        {/* img container */}
+        <div className="img-container flex justify-center mb-20">
+          {/* <img src={Logo} alt= 'logo' className="h-28"/> */}
+          <Avatar alt="Remy Sharp" src="" />
         </div>
-    </DashboardLayout>
-  )
-}
+        <div className="accordions grid grid-cols-1 lg:grid-cols-1 gap-12">
+          {/* accordian 1 */}
+          <CompanyInfoAccordion />
 
-export default CompanyDetails
+          {/* accordian 2 */}
+          <CompanySettingsAccordion />
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default CompanyDetails;
