4fefbbb308c26dcce832f21baf4fed908db07e03
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-08-02 01:05:31 +0100

    add employee validate

--- src/Settings/Components/Organization/Designations/AddDesignationDrawer.js
+++ src/Settings/Components/Organization/Designations/AddDesignationDrawer.js
@@ -1,8 +1,26 @@
 import React from "react";
-
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { motion } from "framer-motion";
 
 const AddDesignationDrawer = ({ handleDrawer }) => {
+  // Handle form
+  const initialValues = {
+    name: "",
+    department: "",
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Field is required!"),
+    department: Yup.string().required("Field is required!"),
+  });
+
+  const onSubmit = (values, onSubmitProps) => {
+    console.log("Form data", values);
+    onSubmitProps.setSubmitting(false);
+    onSubmitProps.resetForm();
+  };
+
   return (
     <motion.div
       initial={{ x: 500 }}
@@ -28,40 +46,58 @@ const AddDesignationDrawer = ({ handleDrawer }) => {
       <div className="mt-4 text-accent">
         {/* form */}
         <div className="px-6 mt-4">
-          <form className="text-accent mt-6 grid grid-cols-1 gap-8">
-            <div>
-              <div className="input-container w-full">
-                <label className="text-sm mb-2 block">Designation Name</label>
-                <input
-                  type="text"
-                  placeholder="eg. UI/UX Designer"
-                  className="w-full bg-transparent rounded-md p-2 border border-gray-400 focus:outline-none "
-                />
-              </div>
-            </div>
-            <div>
-              <div className="input-container w-full">
-                <label className="text-sm mb-2 block">Department</label>
-                <select
-                  type="text"
-                  placeholder="eg. johndoe@gmail.com"
-                  className="w-full bg-transparent rounded-md p-2 border border-gray-400 focus:outline-none "
-                >
-                  <option>Department 1</option>
-                </select>
-              </div>
-            </div>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+            validateOnMount
+          >
+            {(formik) => {
+              return (
+                <Form>
+                  <div className="text-accent mt-6 grid grid-cols-1 gap-8 add-designation-form">
+                    <div className="input-container">
+                      <label>Designation Name</label>
+                      <Field
+                        type="text"
+                        name="name"
+                        placeholder="eg. UI/UX Designer"
+                      />
+                      <ErrorMessage name="name" component="span" />
+                    </div>
 
-            {/* ctrl btns */}
-            <div className="form-buttons flex justify-between mt-2">
-              <button className="py-2 px-4 rounded text-sm font-medium">
-                Cancel
-              </button>
-              <button className="py-2 px-4 bg-caramel rounded text-sm text-white font-medium">
-                Submit
-              </button>
-            </div>
-          </form>
+                    <div className="input-container">
+                      <label>Department</label>
+                      <Field as="select" name="department">
+                        <option value="">Select department</option>
+                        <option value="Department-2">Department 2</option>
+                        <option value="Department-3">Department 2</option>
+                      </Field>
+                      <ErrorMessage name="department" component="span" />
+                    </div>
+
+                    {/* ctrl btns */}
+                    <div className="form-buttons flex justify-between mt-2">
+                      <button
+                        type="button"
+                        className="py-2 px-4 rounded text-sm font-medium"
+                        onClick={() => handleDrawer("")}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="submit"
+                        disabled={!formik.isValid || formik.isSubmitting}
+                        className="button"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                  </div>
+                </Form>
+              );
+            }}
+          </Formik>
         </div>
       </div>
     </motion.div>
