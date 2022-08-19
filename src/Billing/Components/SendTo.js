252e30eeb6698bfdcb5164a858ce4b4012647ba6
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-08-19 16:13:11 +0100

    wallet

--- src/Billing/Components/SendTo.js
+++ src/Billing/Components/SendTo.js
@@ -1,68 +1,63 @@
-import { Modal, TextField } from "@mui/material";
+import { Modal } from "@mui/material";
 import React from "react";
 import Themes from "../../Themes/Themes";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
-import Checkbox from "@mui/material/Checkbox";
-import Autocomplete from "@mui/material/Autocomplete";
-import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
-const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
-const checkedIcon = <CheckBoxIcon fontSize="small" />;
-
-const employee = [
-  { id: 1, name: "Godswill Omenuko" },
-  { id: 2, name: "Isaac Odeh" },
-  { id: 3, name: "Peter Obi" },
-];
 const SendTo = ({ open, handleClose }) => {
+  const onSubmit = (values) => {};
+
   return (
     <Modal open={open} onClose={handleClose}>
       <Themes>
         <div className="CModal" style={{ maxWidth: 400 }}>
-          <i
-            className="fas fa-times cursor-pointer text-xl flex justify-end mb-8"
-            onClick={handleClose}
-          ></i>
-          <form>
-            <Autocomplete
-              multiple
-              id="checkboxes-tags-demo"
-              options={employee}
-              disableCloseOnSelect
-              getOptionLabel={(option) => option.name}
-              renderOption={(props, option, { selected }) => (
-                <li {...props}>
-                  <Checkbox
-                    icon={icon}
-                    checkedIcon={checkedIcon}
-                    style={{ marginRight: 8 }}
-                    checked={selected}
-                  />
-                  {option.name}
-                </li>
-              )}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Select employee"
-                  placeholder="Employee"
+          <div className="flex items-center justify-between mb-12">
+            <h5 className="text-lg font-medium">Input Email</h5>
+            <i
+              className="fas fa-times cursor-pointer text-xl flex justify-end"
+              onClick={handleClose}
+            ></i>
+          </div>
+          <Formik
+            initialValues={{
+              email: "",
+            }}
+            validationSchema={Yup.object({
+              email: Yup.string()
+                .required("Field is Required!")
+                .email("Invalid email format"),
+            })}
+            onSubmit={onSubmit}
+          >
+            <Form>
+              <div>
+                <Field
+                  type="email"
+                  name="email"
+                  className="w-full py-3 rounded border border-gray-400 px-2 focus:outline-none placeholder:text-accent"
+                  placeholder="johndoe@gmail"
                 />
-              )}
-            />
+                <ErrorMessage
+                  name="email"
+                  component="span"
+                  className="showErrorMsg"
+                />
+              </div>
 
-            <div className="flex items-center justify-around mt-5">
-              <button
-                type="button"
-                onClick={handleClose}
-                className="transparentButton"
-              >
-                Cancel
-              </button>
-              <button type="submit" className="button">
-                Submit
-              </button>
-            </div>
-          </form>
+              <div className="flex items-center justify-around mt-5">
+                <button
+                  type="button"
+                  onClick={handleClose}
+                  className="transparentButton"
+                >
+                  Cancel
+                </button>
+                <button type="submit" className="button">
+                  Submit
+                </button>
+              </div>
+            </Form>
+          </Formik>
         </div>
       </Themes>
     </Modal>
