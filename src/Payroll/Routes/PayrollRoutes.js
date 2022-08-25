f0977e7840755e68a4dd26e2c25fc210bd1e5858
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-08-25 10:45:42 +0100

    payroll setup

---
+++ src/Payroll/Routes/PayrollRoutes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const PayrollRoutes = () => {
+  return (
+    <>
+     <Routes>
+        
+     </Routes>
+    </>
+  )
+}
+
+export default PayrollRoutes
