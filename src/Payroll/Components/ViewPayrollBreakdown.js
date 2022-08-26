e1a3be881d76662289b13d79e6e4527426dcb9fb
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-08-26 23:37:37 +0100

    payroll

--- src/Payroll/Components/ViewPayrollBreakdown.js
+++ src/Payroll/Components/ViewPayrollBreakdown.js
@@ -15,10 +15,54 @@ const ViewPayrollBreakdown = ({ open, handleClose }) => {
             ></i>
           </div>
 
-          <div className="text-sm mt-5">
+          <div className="text-sm mt-5 font-medium">
             <div className="bg-mainBg flex items-center justify-between px-5 py-2">
               <span> Employee Name</span>
               <span>Ruth Godwin</span>
+            </div>
+            <div className="bg-mainBg flex items-center justify-between px-5 py-2 mt-3">
+              <span> Employee ID</span>
+              <span>HC54321</span>
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 my-6">
+           
+                <table className="payroll-table view">
+                  <thead>
+                    <tr>
+                      <th>Earnings</th>
+                      <th>Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Total Gross</td>
+                      <td>N0.00</td>
+                    </tr>
+                    <tr>
+                      <td>Total Net</td>
+                      <td>N0.00</td>
+                    </tr>
+                    <tr>
+                      <td>Total Bonuses</td>
+                      <td>N0.00</td>
+                    </tr>
+                  </tbody>
+                </table>
+                  
+                  <table>
+                     
+                  </table>
+           
+            </div>
+
+            <div className="bg-mainBg flex items-center justify-between px-5 py-2">
+              <span> Total</span>
+              <span>N0.00</span>
+            </div>
+            <div className="bg-mainBg flex items-center justify-between px-5 py-2 mt-3">
+              <span>Account Number</span>
+              <span>xxxxxxxxxx</span>
             </div>
           </div>
         </div>
