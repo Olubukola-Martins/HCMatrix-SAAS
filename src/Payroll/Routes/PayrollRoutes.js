b6731955e5759b26e0ef41a780b42d82e27e281c
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-08-27 01:04:04 +0100

    payroll rollback

--- src/Payroll/Routes/PayrollRoutes.js
+++ src/Payroll/Routes/PayrollRoutes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import PayrollBreakdown from "../Pages/PayrollBreakdown";
+import PayrollComparison from "../Pages/PayrollComparison";
 import PayrollReview from "../Pages/PayrollReview";
 
 const PayrollRoutes = () => {
@@ -9,6 +10,7 @@ const PayrollRoutes = () => {
       <Routes>
         <Route path="/payroll-review" element={<PayrollReview />} />
         <Route path="/payroll-breakdown" element={<PayrollBreakdown />} />
+        <Route path="/payroll-comparison" element={<PayrollComparison />} />
       </Routes>
     </>
   );
