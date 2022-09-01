a008184bdda6805bfb63a704516fbee6ba0cc393
kaasikodes <thisbroisfresh@gmail.com>
2022-09-01 15:46:01 +0100

    conflict fix

--- src/App.js
+++ src/App.js
@@ -9,15 +9,11 @@ function App() {
   return (
     <Router>
       <AuthRoutes />
-<<<<<<< HEAD
+
       <SettingRoutes />
       <BillingRoutes />
       <PayrollRoutes />
-=======
-      <SettingRoutes/>
-        <BillingRoutes/>
-        <PayrollRoutes/>
->>>>>>> 1952f40d62968393823f495afa9bc4cd19e63137
+
       {/* <Routes>
       <Route path="*" element={<NotFoundPage /> }/>
       </Routes> */}
