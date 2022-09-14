fbf71356483ebe04a443329921aee3bd91d86b07
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-09-14 16:36:58 +0100

    self service

--- src/Self_Service/Pages/Requisition.js
+++ src/Self_Service/Pages/Requisition.js
@@ -6,15 +6,24 @@ const Requisition = () => {
   return (
     <DashboardLayout>
       <div>
-        <div className="flex items-center gap-2 mb-10">
+        <div className="flex items-center gap-2">
           <Link to="/self-service/home" className="hover:text-caramel">
             <i className="ri-arrow-left-s-line text-xl"></i>
           </Link>
           <h5 className="font-black text-lg">Asset Requisition</h5>
         </div>
-        <div>
-            
+        <div className="flex items-center gap-3 mt-4 md:mt-0 justify-start md:justify-end">
+          <button className="button">New Requisition</button>
+          <button
+            className="transparentButton"
+            style={{ color: "var(--caramel)" }}
+          >
+            Add / Replace Assets
+          </button>
         </div>
+
+
+        
       </div>
     </DashboardLayout>
   );
