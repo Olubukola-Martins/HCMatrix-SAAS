a7e3e65eece59a3338a0ecb6fb8b7226fa2ce69f
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-09-17 22:29:40 +0100

    monetary

--- src/Self_Service/Pages/Monetary.js
+++ src/Self_Service/Pages/Monetary.js
@@ -6,50 +6,40 @@ import NewRequisition from "../Components/Requisition/NewRequisition";
 import RequisitionDetails from "../Components/Requisition/RequisitionDetails";
 
 const Monetary = () => {
-  const [newRequisitionModal, setNewRequisitionModal] = useState(false);
-  const [addAssets, setAddAssets] = useState(false);
+  const [newMonetaryModal, setNewMonetaryModal] = useState(false);
   const [requisitionDetailModal, setRequisitionDetailModal] = useState(false);
 
   return (
     <DashboardLayout>
       {/* modal components */}
       <NewRequisition
-        open={newRequisitionModal}
-        handleClose={() => setNewRequisitionModal(false)}
+        open={newMonetaryModal}
+        handleClose={() => setNewMonetaryModal(false)}
       />
-      <Add_ReplaceAsset
-        open={addAssets}
-        handleClose={() => setAddAssets(false)}
-      />
+     
 
-      <RequisitionDetails
+      {/* <RequisitionDetails
         open={requisitionDetailModal}
         handleClose={() => setRequisitionDetailModal(false)}
-      />
+      /> */}
 
       <div className="Container">
         <div className="flex items-center gap-2">
           <Link to="/self-service/home" className="hover:text-caramel">
             <i className="ri-arrow-left-s-line text-xl"></i>
           </Link>
-          <h5 className="font-black text-lg">Asset Requisition</h5>
+          <h5 className="font-black text-lg">Monetary Request</h5>
         </div>
         <div className="flex items-center gap-3 mt-4 md:mt-0 justify-start md:justify-end">
           <i className="ri-download-2-line text-xl cursor-pointer"></i>
           <i className="fa-solid fa-file-export cursor-pointer"></i>
           <button
             className="button"
-            onClick={() => setNewRequisitionModal(true)}
+            onClick={() => setNewMonetaryModal(true)}
           >
-            New Requisition
+           New Request
           </button>
-          <button
-            onClick={() => setAddAssets(true)}
-            className="transparentButton"
-            style={{ color: "var(--caramel)" }}
-          >
-            Add / Replace Assets
-          </button>
+        
         </div>
 
         <table className="payroll-table text-accent mt-6">
@@ -58,9 +48,9 @@ const Monetary = () => {
               <th>
                 <input type="checkbox" />
               </th>
-              <th>Requisition Date</th>
-              <th>Requisition Type</th>
+              <th>Date</th>
               <th>Title</th>
+              <th>Purpose</th>
               <th>Amount</th>
               <th>Status</th>
               <th>Action</th>
@@ -73,9 +63,9 @@ const Monetary = () => {
                   <input type="checkbox" />
                 </td>
                 <td>DD/MM/YY</td>
-                <td>Asset Request</td>
-                <td>Laptop Request</td>
-                <td>HP EliteBook</td>
+                <td>Lunch</td>
+                <td>Lunch for Godswill</td>
+                <td>N0.00</td>
                 <td>Pending</td>
                 <td className="flex items-center justify-center gap-3">
                   <i
--- src/Self_Service/Routes/SelfServiceRoutes.js
+++ src/Self_Service/Routes/SelfServiceRoutes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Monetary from '../Pages/Monetary'
 import Reimbursements from '../Pages/Reimbursements'
 import Requisition from '../Pages/Requisition'
 import SelfServiceHome from '../Pages/SelfServiceHome'
@@ -11,6 +12,7 @@ const SelfServiceRoutes = () => {
           <Route path="/self-service/home" element={<SelfServiceHome/>}/>
           <Route path="/self-service/requisition" element={<Requisition />}/>
           <Route path="/self-service/reimbursements" element={<Reimbursements />}/>
+          <Route path="/self-service/monetary" element={<Monetary />}/>
       </Routes>
     </>
   )
