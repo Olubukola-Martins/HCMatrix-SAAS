78e6e86b54f4a34f56fdc4918d544546461cd830
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-09-19 19:44:54 +0100

    assets

--- src/Self_Service/Pages/AssetOverview.js
+++ src/Self_Service/Pages/AssetOverview.js
@@ -1,4 +1,5 @@
 import React from "react";
+import roundgraph from "../Assets/Images/roundgraph.svg";
 
 const AssetOverview = () => {
   const requestStyle =
@@ -6,6 +7,10 @@ const AssetOverview = () => {
 
   return (
     <div>
+        <div className="flex items-center gap-3 justify-end">
+            <button className="button">Add Asset</button>
+            <button className="transparentButton" style={{color: "var(--caramel)"}} >Add Asset Type</button>
+        </div>
       <div className="grid grid-cols-1 md:grid-cols-4">
         <div className="col-span-3">graph</div>
         <div className="bg-mainBg border mt-4 rounded-lg text-sm shadow ">
@@ -29,19 +34,57 @@ const AssetOverview = () => {
         </div>
       </div>
 
-       <h3>Asset Type</h3>
-      <div className="grid grid-cols-1 md:grid-cols-4">
-           <div>
-             {[1,2,3,4,5,6].map(item => <div key={item} className="border rounded-md p-3">
-                      <div className="rounded-md bg-mainBg shadow px-1 py-2">
-                           
-                      </div>
-             </div>)}
-           </div>
+      <h3 className="pt-10 pb-6 font-semibold">Asset Type</h3>
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+        <div className="col-span-3">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {[1, 2, 3, 4, 5, 6].map((item) => (
+              <div
+                key={item}
+                className="border rounded-md p-2 shadow-sm bg-card"
+              >
+                <div className="rounded-md bg-mainBg shadow p-3 border">
+                  <p className="text-sm py-2">Furniture & Fittings</p>
+                  <h2 className="font-semibold">0</h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
 
-           <div>
+        <div className="border rounded-md p-2 shadow">
+          <p>Asset by Status</p>
+          <div className="flex justify-center">
+            <img src={roundgraph} alt="graph" className="h-36" />
+          </div>
 
-           </div>
+          <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+              <div className="w-3 h-3 rounded bg-caramel" />
+              <span className="text-sm">0 Assigned</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <div
+                className="w-3 h-3 rounded"
+                style={{ background: "#FFA600" }}
+              />
+              <span className="text-sm">0 Unassigned</span>
+            </div>
+          </div>
+          <div className="flex justify-between mt-1">
+            <div className="flex items-center gap-2">
+              <div
+                className="w-3 h-3 rounded"
+                style={{ background: "#FFF0F0" }}
+              />
+              <span className="text-sm">0 Under Repair</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="w-3 h-3 rounded bg-card" />
+              <span className="text-sm">0 Condemned</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
