972acdf5939cef02d7cfe5db4d3acdb490ab6be6
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-09-21 13:59:10 +0100

    assets

--- src/Self_Service/Pages/Assets/AssetDetails.js
+++ src/Self_Service/Pages/Assets/AssetDetails.js
@@ -2,22 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import DashboardLayout from "../../../Layout/DashboardLayout";
 import SelfServiceSubNav from "../../Components/SelfServiceSubNav";
+import placeholder from "../../Assets/Images/placeholder.png"
 
 const AssetDetails = () => {
   return (
     <DashboardLayout>
-        <SelfServiceSubNav />
+      <SelfServiceSubNav />
       <div className="Container">
-   
         <div className="flex items-center gap-2">
-          <Link to="/self-service/home" className="hover:text-caramel">
+          <Link to="/self-service/assets/1" className="hover:text-caramel">
             <i className="ri-arrow-left-s-line text-xl"></i>
           </Link>
           <h5 className="font-black text-lg">Asset Details</h5>
         </div>
 
-        <div>
-            
+        <div className="flex md:items-center flex-col md:flex-row justify-start md:justify-between mt-4">
+          <h3 className="font-medium text-lg">Hp EliteBook</h3>
+          <div className="flex items-center gap-3">
+            <button className="button">Edit</button>
+            <button
+              className="transparentButton"
+              style={{ color: "var(--neutral)" }}
+            >
+            Delete
+            </button>
+            <button className="transparentButton">Add Document</button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+           <div>
+           <img src="https://via.placeholder.com/350x150"/>
+           </div>
+           <div>
+
+           </div>
+           <div>
+
+           </div>
         </div>
       </div>
     </DashboardLayout>
