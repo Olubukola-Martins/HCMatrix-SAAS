056ebe9018baba95d95275953f1cd56f13c349ee
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-09-21 15:58:42 +0100

    assets

--- src/Self_Service/Pages/Assets/AssetDetails.js
+++ src/Self_Service/Pages/Assets/AssetDetails.js
@@ -5,7 +5,7 @@ import SelfServiceSubNav from "../../Components/SelfServiceSubNav";
 import placeholder from "../../Assets/Images/placeholder.png";
 
 const AssetDetails = () => {
-    const [tap, setTap] = useState("overview");
+  const [tap, setTap] = useState("History");
   const listStyle =
     "flex items-center justify-between cursor-pointer group border-b pb-2 px-3";
   return (
@@ -99,36 +99,26 @@ const AssetDetails = () => {
         </div>
 
         {/* Taps */}
-        <div className="flex items-center gap-5 font-medium border-b-2 text-sm mt-4 mb-5">
+        <div className="flex items-center gap-5 font-medium border-b-2 text-sm mb-5 mt-10">
           <h5
-            onClick={() => setTap("overview")}
+            onClick={() => setTap("History")}
             className={
-              tap === "overview"
+              tap === "History"
                 ? "cursor-pointer hover:text-caramel pb-4 border-b-2 border-caramel"
                 : "cursor-pointer hover:text-caramel pb-4"
             }
           >
-            Asset Overview
+            Assignee History
           </h5>
           <h5
-            onClick={() => setTap("list")}
+            onClick={() => setTap("Documents")}
             className={
-              tap === "list"
+              tap === "Documents"
                 ? "cursor-pointer hover:text-caramel pb-4 border-b-2 border-caramel"
                 : "cursor-pointer hover:text-caramel pb-4"
             }
           >
-            Asset List
-          </h5>
-          <h5
-            onClick={() => setTap("type")}
-            className={
-              tap === "type"
-                ? "cursor-pointer hover:text-caramel pb-4 border-b-2 border-caramel"
-                : "cursor-pointer hover:text-caramel pb-4"
-            }
-          >
-            Asset Type
+            Documents
           </h5>
         </div>
       </div>
