5e1abdc1f662880ddab4ccf98c70034ff0c5f3e2
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-09-22 11:44:16 +0100

    assets

--- src/Self_Service/Pages/Assets/AssetList.js
+++ src/Self_Service/Pages/Assets/AssetList.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import { Popover } from "@mui/material";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Themes from "../../../Themes/Themes";
 
 const AssetList = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
   return (
-    <div>AssetList</div>
-  )
-}
+    <div>
+      <div className="flex justify-end items-center">
+        <button className="button flex items-center gap-2">
+          <span>Add Asset</span>{" "}
+          <i className="ri-arrow-down-s-line text-base"></i>
+        </button>
+      </div>
+      <table className="payroll-table text-accent mt-6">
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" />
+            </th>
+            <th>Asset Name</th>
+            <th>Asset ID</th>
+            <th>Asset Type</th>
+            <th>Status</th>
+            <th>Color</th>
+            <th>Serial No</th>
+            <th>Assigned to</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {[1, 2, 3, 4].map((item) => (
+            <tr key={item}>
+              <td>
+                <input type="checkbox" />
+              </td>
+              <td>
+                <Link
+                  to="/self-service/assets-details"
+                  className="hover:text-caramel"
+                >
+                  Hp EliteBook
+                </Link>
+              </td>
+              <td>000</td>
+              <td>Computer Accessories</td>
+              <td>Assigned</td>
+              <td>Red</td>
+              <td>0000</td>
+              <td>Ruth Godwin</td>
+              <td>
+                <i
+                  onClick={(e) => setAnchorEl(e.currentTarget)}
+                  class="ri-more-2-fill text-lg cursor-pointer hover:text-caramel"
+                ></i>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-export default AssetList
+      {/* Action popover */}
+      <Popover
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "top",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "top",
+        }}
+      >
+        <Themes>
+          <div className="py-3 px-4 text-sm font-medium rounded-md flex flex-col bg-card">
+            <span className="cursor-pointer hover:text-caramel">Edit</span>
+            <span className="cursor-pointer hover:text-caramel py-1">
+              Delete
+            </span>
+            <span className="cursor-pointer hover:text-caramel py-1">
+              Download
+            </span>
+            <span className="cursor-pointer hover:text-caramel py-1">
+              Add Document
+            </span>
+          </div>
+        </Themes>
+      </Popover>
+    </div>
+  );
+};
+
+export default AssetList;
