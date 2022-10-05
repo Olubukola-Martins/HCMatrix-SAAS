15e1d35e6d905797a01a90cec5026d4daee075f4
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-10-05 21:57:15 +0100

    loan policy

--- src/Self_Service/Pages/Loan/LoanPolicies.js
+++ src/Self_Service/Pages/Loan/LoanPolicies.js
@@ -1,8 +1,14 @@
-import React from "react";
+import { Switch } from "antd";
+import React, { useState } from "react";
 import DashboardLayout from "../../../Layout/DashboardLayout";
 import SelfServiceSubNav from "../../Components/SelfServiceSubNav";
 
 const LoanPolicies = () => {
+  const [bankDSwitch, setBankDSwitch] = useState(false);
+  const [paymentPlanSwitch, setPaymentPlanSwitch] = useState(false);
+  const [loanTypeSwitch, setLoanTypeSwitch] = useState(false);
+  const [loanPec, setLoanPec] = useState(false);
+
   const boxStyle = "px-4 py-3 shadow rounded-md bg-mainBg";
   const boxTitle = "font-medium text-base pb-1";
   const inputStyle =
@@ -17,16 +23,128 @@ const LoanPolicies = () => {
         </div>
 
         <div className="">
-          <div className="bg-card px-5 py-7  rounded-md mt-7 grid grid-cols-1 md:grid-cols-2 gap-5 text-accent">
-            <div>
+          <div className="bg-card px-5 py-7  rounded-md mt-7 grid grid-cols-1 md:grid-cols-2 gap-7 text-accent">
+            <div className="flex flex-col gap-4">
               <div className={boxStyle}>
-                <select name="" id="">
+                <select name="" id="" className={inputStyle}>
                   <option value="">Select Work Flow Approval</option>
                 </select>
               </div>
+
+              <div className={`${boxStyle} text-sm`}>
+                <div className="flex items-center justify-between">
+                  <h5 className={boxTitle}>Upload company's Bank Details</h5>
+                  <Switch
+                    checked={bankDSwitch}
+                    onChange={(value) => {
+                      setBankDSwitch(value);
+                    }}
+                  />
+                </div>
+                <p className="text-sm pt-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Tempor magna id fames cursus luctus scel
+                </p>
+
+                {bankDSwitch && (
+                  <div>
+                    <form className="flex flex-col gap-4 mt-5">
+                      <input
+                        type="text"
+                        placeholder="Bank Name"
+                        className={inputStyle}
+                      />
+                      <input
+                        type="text"
+                        placeholder="Account Name"
+                        className={inputStyle}
+                      />
+                      <input
+                        type="number"
+                        placeholder="Account Number"
+                        className={inputStyle}
+                      />
+                    </form>
+                    <div className="flex items-center justify-between mt-6 mb-2">
+                      <button
+                        onClick={() => setBankDSwitch(false)}
+                        className="transparentButton"
+                        type="button"
+                      >
+                        Cancel
+                      </button>
+                      <button className="button" type="submit">
+                        Save
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
+
+              <div className={`${boxStyle} text-sm`}>
+                <div className="flex items-center justify-between">
+                  <h5 className={boxTitle}>Set Payment Plan</h5>
+                  <Switch
+                    // checked={bankDSwitch}
+                    onChange={(value) => {
+                      setPaymentPlanSwitch(value);
+                    }}
+                  />
+                </div>
+                <p className="text-sm pt-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Tempor magna id fames cursus luctus scel
+                </p>
+              </div>
             </div>
 
-            <div></div>
+            <div className="flex flex-col gap-4">
+              <div className={`${boxStyle} text-sm`}>
+                <div className="flex items-center justify-between">
+                  <h5 className={boxTitle}>Set Loan Types</h5>
+                  <Switch
+                    checked={loanTypeSwitch}
+                    onChange={(value) => {
+                      setLoanTypeSwitch(value);
+                    }}
+                  />
+                </div>
+                <p className="text-sm pt-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Tempor magna id fames cursus luctus scel
+                </p>
+
+                {loanTypeSwitch && (
+                  <form className="mt-4">
+                    <input
+                      type="text"
+                      placeholder="E.g Car Loan"
+                      className={inputStyle}
+                    />
+                    <div className="flex items-center justify-between mt-6 mb-2">
+                      <button type="button" onClick={() =>  setLoanTypeSwitch(false)} className="transparentButton">Cancel</button>
+                      <button type="submit" className="button">Save</button>
+                    </div>
+                  </form>
+                )}
+              </div>
+
+              <div className={`${boxStyle} text-sm`}>
+                <div className="flex items-center justify-between">
+                  <h5 className={boxTitle}>Set Maximum Loan Percentage</h5>
+                  <Switch
+                    // checked={bankDSwitch}
+                    onChange={(value) => {
+                      setLoanPec(value);
+                    }}
+                  />
+                </div>
+                <p className="text-sm pt-2">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Tempor magna id fames cursus luctus scel
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
