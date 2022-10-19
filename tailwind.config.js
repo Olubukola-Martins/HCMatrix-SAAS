7ac7bc81621272be10ecef8b2264d440048f4f32
kaasikodes <thisbroisfresh@gmail.com>
2022-10-19 16:53:37 +0100

    initial-commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      backgroundColor: {
-        caramel: "var(--caramel)",
-        card: "var(--card)",
-        faded: "var(--faded)",
-        mainBg: "var(--background)",
-        neutral: "var(--neutral)",
-      },
-      colors: {
-        caramel: "var(--caramel)",
-        card: "var(--card)",
-        accent: "var(--accent)",
-        neutral: "var(--neutral)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
