cf2939fdbd70175cbb7f7d241181e0b913afd3f2
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-11-06 01:08:32 +0100

    login

--- src/Auth/Pages/Login.tsx
+++ src/Auth/Pages/Login.tsx
@@ -1,33 +1,11 @@
 import peachBg from "../Assets/Images/peachBg.png";
 import lightBg from "../Assets/Images/lightBg.png";
+import logo from "../Assets/Images/logo.png";
 
 export const Login = () => {
   return (
-    <div className="flex">
-      <div
-        className="Container w-full h-screen text-white"
-        style={{ backgroundImage: `url(${peachBg})` }}
-      >
-        <div>
-          <div className="text-center">
-            <h2 className="text-white">Welcome To HCMatrix!</h2>
-            <p>
-              Already have an account? <br className="hidden md:flex" /> please
-              login with your personal information to stay
-              <br className="hidden md:flex" /> connected with us.
-            </p>
-            <button className="border border-white rounded px-3 py-1 text-white">
-              Sign In
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        className="Container w-full h-screen"
-        style={{ backgroundImage: `url(${lightBg})` }}
-      >
-        Hello
-      </div>
+    <div>
+      login
     </div>
   );
 };
--- src/Auth/Pages/Register.tsx
+++ src/Auth/Pages/Register.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import peachBg from "../Assets/Images/peachBg.png";
+import lightBg from "../Assets/Images/lightBg.png";
+import logo from "../Assets/Images/logo.png";
 
 export const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="flex">
+      <div
+        className="Container w-full h-screen text-white py-2"
+        style={{ backgroundImage: `url(${peachBg})` }}
+      >
+        <div>
+          <img src={logo} alt="logo" className="h-14" />
+          <div
+            className="flex items-center justify-center"
+            style={{ height: "80vh" }}
+          >
+            <div className="text-center">
+              <h2 className="text-white text-xl font-bold">
+                Welcome To HCMatrix!
+              </h2>
+              <p className="pt-6 pb-4">
+                Already have an account? <br className="hidden md:flex" />
+                please login with your personal information to stay
+                <br className="hidden md:flex" /> connected with us.
+              </p>
+              <button className="border border-white rounded px-16 py-1 text-white font-medium text-base">
+                Sign In
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="Container w-full h-screen py-2"
+        style={{ backgroundImage: `url(${lightBg})` }}
+      >
+        Hello
+      </div>
+    </div>
+  );
+};
