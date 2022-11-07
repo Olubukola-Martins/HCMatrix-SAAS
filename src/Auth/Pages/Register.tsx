ae9fdd847d5f5f198897e95b1c264c0ef95cfe47
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-11-07 23:21:00 +0100

    login

--- src/Auth/Pages/Register.tsx
+++ src/Auth/Pages/Register.tsx
@@ -18,6 +18,7 @@ import {
   emailValidationRules,
   generalValidationRules,
 } from "../../FormHelpers/validation";
+import { Link } from "react-router-dom";
 
 export const Register = () => {
   const industryData = [
@@ -61,9 +62,9 @@ export const Register = () => {
                 <br className="hidden md:flex" /> connected with us.
               </p>
               <div className="flex justify-center">
-                <button className="border justify-center flex items-center gap-3 border-white rounded px-16 py-1 text-white hover:border-gray-700 font-medium text-base transition duration-300 ease-in-out">
+                <Link to="/login" className="border justify-center flex items-center gap-3 border-white rounded px-16 py-1 text-white hover:border-gray-700 font-medium text-base transition duration-300 ease-in-out">
                   <i className="ri-arrow-left-line"></i> <span>Sign In</span>
-                </button>
+                </Link>
               </div>
             </div>
           </div>
