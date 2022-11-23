3a1316ae2b4e34cb37e42b2e4cc3e53b6eb8e48f
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-11-23 15:25:27 +0100

    finish delegation refactor

--- src/Settings/Components/Organization/Delegations/AddDelegation.tsx
+++ src/Settings/Components/Organization/Delegations/AddDelegation.tsx
@@ -1,190 +1,15 @@
-// import React from "react";
-// import * as Yup from "yup";
-// import { motion } from "framer-motion";
-// import { ErrorMessage, Field, Form, Formik } from "formik";
-
-import { Form, Input, Modal, Select } from "antd";
+import { DatePicker, Form, Input, Modal, Select } from "antd";
+import TextArea from "antd/lib/input/TextArea";
+import { useState } from "react";
 import { IModalProps } from "../../../../AppTypes/Component";
 import {
   generalValidationRules,
   textInputValidationRules,
 } from "../../../../FormHelpers/validation";
-
-// const AddDelegationDrawer = ({ handleDrawer }) => {
-//   const initialValues = {
-//     delegator: "",
-//     delegatee: "",
-//     notification: [],
-//     description: "",
-//     delegation_type: "",
-//     delegation_duration: "",
-//   };
-
-//   const validationSchema = Yup.object({
-//     delegator: Yup.string().required("Field is required!"),
-//     delegatee: Yup.string().required("Field is required!"),
-//     notification: Yup.array().required("At least one must be checked"),
-//     description: Yup.string().required("Field is required!"),
-//     delegation_type: Yup.string().required("Field is required!"),
-//   });
-
-//   const onSubmit = (values, onSubmitProps) => {
-//     console.log("Form data", values);
-//     onSubmitProps.setSubmitting(false);
-//     onSubmitProps.resetForm();
-//   };
-
-//   return (
-//     <motion.div
-//       initial={{ x: 500 }}
-//       animate={{
-//         x: 0,
-//       }}
-//       transition={{ ease: "easeIn" }}
-//       exit={{ x: 500 }}
-//       className="w-96 fixed overflow-y-auto mode_color right-0 drop-shadow-lg z-50 cursor-move pb-8"
-//       drag
-//       style={{ height: "28rem" }}
-//     >
-//       <div className="flex justify-between text-xl items-center font-light py-2 px-4 mt-4">
-//         <h5 className="text-accent">Add Delegation</h5>
-//         <i
-//           className="fa fa-times cursor-pointer"
-//           aria-hidden="true"
-//           onClick={() => handleDrawer("")}
-//         ></i>
-//       </div>
-
-//       <div className="mt-4 text-accent">
-//         <div className="px-6 mt-4">
-//           <Formik
-//             initialValues={initialValues}
-//             validationSchema={validationSchema}
-//             onSubmit={onSubmit}
-//             validateOnMount
-//           >
-//             {(formik) => {
-//               return (
-//                 <Form className="text-accent mt-6 grid grid-cols-1 gap-8">
-//                   <div>
-//                     <div className="input-container w-full">
-//
-//                   </div>
-//                   <div>
-//                     <div className="input-container w-full">
-//
-//                   </div>
-//                   <div>
-//                     <div className="text-sm">
-//                       <label className=" font-bold mb-2 block">Type</label>
-//                       <div className="flex gap-6">
-//                         <div>
-//                           <label
-//                             htmlFor="temporary"
-//                             className="mb-1 block cursor-pointer"
-//                           >
-//                             Temporary
-//                           </label>
-//                           <Field
-//                             id="temporary"
-//                             name="delegation_type"
-//                             type="radio"
-//                             value="Temporary"
-//                           />
-//                         </div>
-//                         <div>
-//                           <label
-//                             htmlFor="Permanent"
-//                             className="mb-1 block cursor-pointer"
-//                           >
-//                             Permanent
-//                           </label>
-//                           <Field
-//                             id="Permanent"
-//                             name="delegation_type"
-//                             type="radio"
-//                             value="Permanent"
-//                           />
-//                         </div>
-//                       </div>
-//                       <div className="input-container mt-3">
-//                         <label className="font-semibold">Enter Duration</label>
-//                         <Field
-//                           type="text"
-//                           name="delegation_duration"
-//                           className="mt-2"
-//                           placeholder="Enter delegation duration"
-//                         />
-//                       </div>
-//                     </div>
-//                   </div>
-//                   <div>
-//                     <div className="input-container w-full text-sm">
-//                       <label className="font-bold mb-2 block">
-//                         Notification
-//                       </label>
-//                       <div className="flex gap-6">
-//                         <div className="flex gap-2 items-center">
-//                           <label htmlFor="N_delegator" className="mb-1 block">
-//                             Delegator
-//                           </label>
-//                           <Field
-//                             type="checkbox"
-//                             id="N_delegator"
-//                             name="notification"
-//                             value="Notify_Delegator"
-//                           />
-//                         </div>
-//                         <div className="flex gap-2 items-center">
-//                           <label htmlFor="N_delegatee" className="mb-1 block">
-//                             Delegatee
-//                           </label>
-//                           <Field
-//                             id="N_delegatee"
-//                             type="checkbox"
-//                             name="notification"
-//                             value="Notify_Delegatee"
-//                           />
-//                         </div>
-//                         <ErrorMessage component="span" name="notification" />
-//                       </div>
-//                     </div>
-//                   </div>
-
-//                   <div>
-//                     <div className="input-container w-full ">
-//                       <label className="text-sm mb-2 block font-bold">
-//                         Description
-//                       </label>
-//                       <Field as="textarea" name="description" rows={4} />
-//                       <ErrorMessage name="description" component="span" />
-//                     </div>
-//                   </div>
-//                   <div className="form-buttons flex justify-between mt-2">
-//                     <button className="py-2 px-4 rounded text-sm font-medium">
-//                       Cancel
-//                     </button>
-//                     <button
-//                       disabled={!formik.isValid || formik.isSubmitting}
-//                       className="button"
-//                       type="submit"
-//                     >
-//                       Save
-//                     </button>
-//                   </div>
-//                 </Form>
-//               );
-//             }}
-//           </Formik>
-//         </div>
-//       </div>
-//     </motion.div>
-//   );
-// };
-
-// export default AddDelegationDrawer;
+const { RangePicker } = DatePicker;
 
 export const AddDelegation = ({ open, handleClose }: IModalProps) => {
+  const [delegationTypeValue, setDelegationTypeValue] = useState("");
   return (
     <Modal
       title="Add Delegation"
@@ -192,7 +17,10 @@ export const AddDelegation = ({ open, handleClose }: IModalProps) => {
       onCancel={() => handleClose(false)}
       footer={null}
     >
-      <Form layout="vertical">
+      <Form
+        layout="vertical"
+        onFinish={(val) => console.log(val)}
+      >
         <Form.Item name="delegator" label="Delegator">
           <Input
             defaultValue="Godswill Smile"
@@ -200,7 +28,6 @@ export const AddDelegation = ({ open, handleClose }: IModalProps) => {
             disabled
           />
         </Form.Item>
-
         <Form.Item
           name="delegatee"
           label="Delegatee"
@@ -226,10 +53,57 @@ export const AddDelegation = ({ open, handleClose }: IModalProps) => {
           label="Delegation Type"
           rules={textInputValidationRules}
         >
-          <Select>
-            <Select.Option> </Select.Option>
+          <Select
+            placeholder="Select delegation type"
+            className="SelectTag w-full"
+            size="large"
+            onChange={(val) => setDelegationTypeValue(val)}
+          >
+            <Select.Option value="permanent">Permanent</Select.Option>
+            <Select.Option value="temporary">Temporary</Select.Option>
           </Select>
         </Form.Item>
+        <Form.Item name="period" label="Select Period">
+          <RangePicker
+            className="generalInputStyle"
+            disabled={
+              delegationTypeValue === ""
+                ? true
+                : delegationTypeValue === "temporary"
+                ? false
+                : true
+            }
+          />
+        </Form.Item>
+        <Form.Item
+          name="permission"
+          label="Permission"
+          rules={generalValidationRules}
+        >
+          <Select
+            showSearch
+            allowClear
+            optionLabelProp="label"
+            className="SelectTag w-full"
+            size="large"
+            placeholder="Select Permission"
+          >
+            {["Payroll approval", "Loan approval"].map((data) => (
+              <Select.Option key={data} value={data} label={data}>
+                {data}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item name="description" label="Description" requiredMark="optional">
+          <TextArea
+            rows={3}
+            className="generalInputStyle"
+            placeholder="Enter Description"
+          />
+        </Form.Item>
+
+        <button className="button">Submit</button>
       </Form>
     </Modal>
   );
--- src/Settings/Pages/Organization/Delegations/DelegationDetail.tsx
+++ src/Settings/Pages/Organization/Delegations/DelegationDetail.tsx
@@ -1,170 +1,154 @@
-import React from "react";
+import { DatePicker, Form, Input, message, Select, Tooltip } from "antd";
+import TextArea from "antd/lib/input/TextArea";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  generalValidationRules,
+  textInputValidationRules,
+} from "../../../../FormHelpers/validation";
 import DashboardLayout from "../../../../Layout/DashboardLayout";
+const { RangePicker } = DatePicker;
 
-const DepartmentDetail = () => {
+const DelegationDetail = () => {
+  const [delegationTypeValue, setDelegationTypeValue] = useState("");
+  const [disable, setDisable] = useState(true);
+
+  const enableEdit = () => {
+    setDisable(!disable);
+    message.success(
+      disable ? "Editing enabled Successfully" : "Editing disabled successfully"
+    );
+  };
+
+  const initialValues = {
+    delegator: "Godswill Smile",
+    delegatee: "Isaac Odeh",
+    delegationType: "permanent",
+    description: "",
+    permission: "Payroll approval",
+  };
   return (
     <DashboardLayout>
-      <div className="h-screen">
+      <div className="Container">
         <div className="bg-card flex justify-between items-center py-2 px-4 rounded-md">
           <div className="flex items-center gap-2 text-accent font-semibold mt-2 pb-1">
-            <Link to="/settings/departments">
+            <Link to="/settings/delegations">
               <i className="ri-arrow-left-line text-lg cursor-pointer hover:text-caramel"></i>
             </Link>
             <h5 className="text-sm">Delegation</h5>
           </div>
-          <div className="flex items-center gap-3">
-            <i className="ri-pencil-fill cursor-pointer text-xl"></i>
-            <i className="ri-question-fill text-xl text-slate-400"></i>
-            <i className="ri-more-fill cursor-pointer text-xl font-semibold"></i>
-          </div>
+          <Tooltip title={disable ? "Enable editing" : "Disable editing"}>
+            {" "}
+            <i
+              onClick={enableEdit}
+              className="ri-pencil-fill cursor-pointer text-xl"
+            ></i>
+          </Tooltip>
         </div>
 
         <div className="bg-card mt-5 pt-4 pb-10 px-4 rounded-md">
           <h3 className="text-accent font-bold text-lg mb-4">
-            Department details
+            Delegation details
           </h3>
-          <div className="mt-4">
-            <form className="text-accent mt-6 grid grid-cols-2 gap-x-24 gap-y-5 w-4/5">
-              <div>
-                <div className="input-container w-full">
-                  <label className="text-sm mb-2 block">Delegation Name</label>
-                  <input
-                    type="text"
-                    placeholder="eg. UI/UX Designer"
-                    className="w-full bg-white rounded-md p-2 border border-gray-400 focus:outline-none "
-                  />
-                </div>
+          <Form
+            layout="vertical"
+            initialValues={initialValues}
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5"
+            disabled={disable}
+          >
+            <Form.Item name="delegator" label="Delegator">
+              <Input disabled className="generalInputStyle" />
+            </Form.Item>
+            <Form.Item
+              name="delegatee"
+              label="Delegatee"
+              rules={generalValidationRules}
+            >
+              <Select
+                showSearch
+                allowClear
+                optionLabelProp="label"
+                className="SelectTag w-full"
+                size="large"
+                placeholder="Select Delegatee"
+              >
+                {["Isaac Odeh", "Obi james"].map((data) => (
+                  <Select.Option key={data} value={data} label={data}>
+                    {data}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+            <Form.Item
+              name="delegationType"
+              label="Delegation Type"
+              rules={textInputValidationRules}
+            >
+              <Select
+                placeholder="Select delegation type"
+                className="SelectTag w-full"
+                size="large"
+                onChange={(val) => setDelegationTypeValue(val)}
+              >
+                <Select.Option value="permanent">Permanent</Select.Option>
+                <Select.Option value="temporary">Temporary</Select.Option>
+              </Select>
+            </Form.Item>
+            <Form.Item name="period" label="Select Period">
+              <RangePicker
+                className="generalInputStyle"
+                disabled={
+                  delegationTypeValue === ""
+                    ? true
+                    : delegationTypeValue === "temporary"
+                    ? false
+                    : true
+                }
+              />
+            </Form.Item>
+            <Form.Item
+              name="permission"
+              label="Permission"
+              rules={generalValidationRules}
+            >
+              <Select
+                showSearch
+                allowClear
+                optionLabelProp="label"
+                className="SelectTag w-full"
+                size="large"
+                placeholder="Select Permission"
+              >
+                {["Payroll approval", "Loan approval"].map((data) => (
+                  <Select.Option key={data} value={data} label={data}>
+                    {data}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+            <div />
+            <Form.Item
+              name="description"
+              label="Description"
+              requiredMark="optional"
+            >
+              <TextArea
+                rows={3}
+                className="generalInputStyle"
+                placeholder="Enter Description"
+                disabled={disable}
+              />
+            </Form.Item>
+            {!disable && (
+              <div className="flex items-center">
+                <button className="button">Save changes</button>
               </div>
-              <div>
-                <div className="input-container w-full">
-                  <label className="text-sm mb-2 block">Modified by</label>
-                  <input
-                    type="text"
-                    placeholder="Josh Potter"
-                    className="w-full bg-white rounded-md p-2 border border-gray-400 focus:outline-none "
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="input-container w-full">
-                  <label className="text-sm mb-2 block">Mail Alias</label>
-                  <input
-                    type="text"
-                    placeholder="eg. johndoe@gmail.com"
-                    className="w-full bg-white rounded-md p-2 border border-gray-400 focus:outline-none "
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="input-container w-full">
-                  <label className="text-sm mb-2 block">Time modified</label>
-                  <input
-                    type="text"
-                    placeholder="24-07-2022"
-                    className="w-full bg-white rounded-md p-2 border border-gray-400 focus:outline-none "
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="input-container w-full">
-                  <label className="text-sm mb-2 block">Department Lead</label>
-                  <select
-                    placeholder="Select"
-                    className="w-full bg-white text-black rounded-md p-2 border border-gray-400 focus:outline-none "
-                  >
-                    <option className="bg-card">Emeka</option>
-                    <option className="bg-card">Chinyere</option>
-                  </select>
-                </div>
-              </div>
-              <div>
-                <div className="input-container w-full invisible">
-                  <label className="text-sm mb-2 block">Time added</label>
-                  <input
-                    type="text"
-                    placeholder="24-07-2022"
-                    className="w-full bg-white rounded-md p-2 border border-gray-400 focus:outline-none "
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="input-container w-full">
-                  <label className="text-sm mb-2 block">
-                    Parent Department
-                  </label>
-                  <select
-                    placeholder="Select"
-                    className="w-full bg-white text-black rounded-md p-2 border border-gray-400 focus:outline-none "
-                  >
-                    <option className="bg-card">Marketing</option>
-                    <option className="bg-card">Development</option>
-                  </select>
-                </div>
-              </div>
-              <div>
-                <div className="input-container w-full invisible">
-                  <label className="text-sm mb-2 block">Time added</label>
-                  <input
-                    type="text"
-                    placeholder="24-07-2022"
-                    className="w-full bg-white rounded-md p-2 border border-gray-400 focus:outline-none "
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="input-container w-full">
-                  <label className="text-sm mb-2 block">Added by</label>
-                  <input
-                    type="text"
-                    placeholder="24-07-2022"
-                    className="w-full bg-white rounded-md p-2 border border-gray-400 focus:outline-none "
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="input-container w-full invisible">
-                  <label className="text-sm mb-2 block">Time added</label>
-                  <input
-                    type="text"
-                    placeholder="24-07-2022"
-                    className="w-full bg-white rounded-md p-2 border border-gray-400 focus:outline-none "
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="input-container w-full">
-                  <label className="text-sm mb-2 block">Time added</label>
-                  <input
-                    type="text"
-                    placeholder="24-07-2022"
-                    className="w-full bg-white rounded-md p-2 border border-gray-400 focus:outline-none "
-                  />
-                </div>
-              </div>
-              <div>
-                <div className="input-container w-full invisible">
-                  <label className="text-sm mb-2 block">Time added</label>
-                  <input
-                    type="text"
-                    placeholder="24-07-2022"
-                    className="w-full bg-white rounded-md p-2 border border-gray-400 focus:outline-none "
-                  />
-                </div>
-              </div>
-
-              {/* ctrl btns - edit n delete */}
-              <div className="form-buttons flex justify-between mt-2">
-                <button className="py-2 px-4 border border-black rounded text-sm hover:opacity-60  font-medium">
-                  Close
-                </button>
-              </div>
-            </form>
-          </div>
+            )}
+          </Form>
         </div>
       </div>
     </DashboardLayout>
   );
 };
 
-export default DepartmentDetail;
+export default DelegationDetail;
