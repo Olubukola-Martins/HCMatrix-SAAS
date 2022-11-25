8c3323acfbbd87d6ac79b095a55217ac9f097ee4
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-11-25 16:37:38 +0100

    employee refactor

--- src/Settings/Components/Organization/EmployeeProfiles/ImportEmployee.tsx
+++ src/Settings/Components/Organization/EmployeeProfiles/ImportEmployee.tsx
@@ -1,42 +1,38 @@
-import { Drawer } from "antd";
+import { Drawer, Form, Upload } from "antd";
 import { IDrawerProps } from "../../../../AppTypes/Component";
 import Themes from "../../../../Themes/Themes";
 
 export const ImportEmployee = ({ open, handleClose }: IDrawerProps) => {
   return (
-    <Drawer placement="right" onClose={() => handleClose(false)} open={open}>
+    <Drawer
+      placement="right"
+      title="Import Employees"
+      onClose={() => handleClose(false)}
+      open={open}
+    >
       <Themes>
-        <div className="border-b font-medium  px-3 py-2 flex items-center justify-between">
-          <h3 className="text-lg">Upload File</h3>
-          <i
-            onClick={() => handleClose(false)}
-            className="ri-close-line text-xl cursor-pointer"
-          ></i>
-        </div>
-        <div className="bg-red-200 text-sm py-2 flex justify-between items-center my-5 px-3">
+        <div className="bg-red-200 text-sm py-2 flex justify-between items-center my-5 px-3 rounded">
           <span>Employees Added: 2</span>
           <span>License count left: 5</span>
         </div>
 
-        <form className="px-3 mt-10 pb-5">
+        <Form className="px-3 mt-10 pb-5">
           <div className="border border-dotted border-slate-500 rounded flex flex-col items-center gap-5 py-10 px-2">
             <p>Choose file to be Imported</p>
-            <input type="file" name="" id="file" className="hidden" />
+
             <div className="flex justify-center">
-              <label
-                htmlFor="file"
-                className="cursor-pointer border border-dotted border-caramel px-2 py-1 rounded text-caramel text-sm flex flex-col gap-1 items-center"
-              >
-                <i className="ri-download-2-line text-lg"></i>
-                <span className="text-xs">Upload File</span>
-              </label>
+              <Upload>
+                <div className="border border-dotted border-caramel px-2 py-1 rounded text-caramel text-sm flex flex-col gap-1 items-center">
+                  <i className="ri-download-2-line text-lg"></i>
+                  <span className="text-xs font-medium">Upload File</span>
+                </div>
+              </Upload>
             </div>
-            <p className="text-center text-xs">
-              [only xls,xlsx and csv formats are supported] <br className="" />
-              Maximum upload file size is 5 MB.
-            </p>
           </div>
-          <div className="flex items-center justify-between mt-7">
+          <p className="text-sm pt-1 font-medium cursor-pointer hover:text-caramel underline">
+            Download template
+          </p>
+          <div className="flex items-center justify-between mt-10">
             <button
               onClick={() => handleClose(false)}
               type="button"
@@ -44,9 +40,11 @@ export const ImportEmployee = ({ open, handleClose }: IDrawerProps) => {
             >
               Cancel
             </button>
-            <button className="button">Submit</button>
+            <button className="button" type="submit">
+              Submit
+            </button>
           </div>
-        </form>
+        </Form>
       </Themes>
     </Drawer>
   );
