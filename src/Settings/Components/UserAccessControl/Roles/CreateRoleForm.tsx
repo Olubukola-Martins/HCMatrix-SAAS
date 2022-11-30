4b3f4df14b7f126b2016c34b57e4b928e39fc270
kaasikodes <thisbroisfresh@gmail.com>
2022-11-30 01:16:23 +0100

    create role form implemented properly

--- src/Settings/Components/UserAccessControl/Roles/CreateRoleForm.tsx
+++ src/Settings/Components/UserAccessControl/Roles/CreateRoleForm.tsx
@@ -104,84 +104,88 @@ const CreateRoleForm = () => {
   };
   return (
     <div>
-      {(!isPSuccess || isPFetching) && <Skeleton active />}
-
-      {isPSuccess && (
-        <Form
-          layout="vertical"
-          requiredMark={false}
-          form={form}
-          onFinish={handleSubmit}
-        >
-          <div className="flex flex-col gap-4">
-            <Typography.Title level={5}>Role name</Typography.Title>
-            <Form.Item
-              name="name"
-              rules={textInputValidationRules}
-              className="w-1/5"
-            >
-              <Input placeholder="role name" />
-            </Form.Item>
-          </div>
-          <div className="flex flex-col gap-4">
-            <Typography.Title level={5}>Assign Permissions</Typography.Title>
-            {/* permission categories */}
-            <div className="flex flex-wrap gap-4 pb-3 border-b ">
-              {data.categories.map((item) => (
-                <div className="" key={item.id}>
-                  <button
-                    type="button"
-                    className={` capitalize hover:bg-caramel hover:border-caramel focus:bg-caramel active:bg-caramel text-white block rounded-full text-sm cursor-pointer px-2 py-1 hover:text-white ${
-                      item.id === selectedCategory
-                        ? "bg-caramel"
-                        : "bg-transparent border border-slate-400 text-slate-400"
-                    }`}
-                    onClick={() => handleCategoryClick(item.id)}
-                  >
-                    {item.name}
-                  </button>
-                </div>
-              ))}
+      <Skeleton
+        active
+        loading={!isPSuccess || isPFetching}
+        paragraph={{ rows: 40 }}
+      >
+        {isPSuccess && (
+          <Form
+            layout="vertical"
+            requiredMark={false}
+            form={form}
+            onFinish={handleSubmit}
+          >
+            <div className="flex flex-col gap-4">
+              <Typography.Title level={5}>Role name</Typography.Title>
+              <Form.Item
+                name="name"
+                rules={textInputValidationRules}
+                className="w-1/5"
+              >
+                <Input placeholder="role name" />
+              </Form.Item>
             </div>
-            <div className="flex justify-end my-2">
-              <Input
-                onChange={handleSearch}
-                className="w-48"
-                placeholder="Search permissions"
-                disabled
-              />
-            </div>
-            <Form.Item name="permissionIds" rules={generalValidationRules}>
-              <Checkbox.Group style={{ width: "100%" }}>
-                <div className="my-6 grid grid-cols-4 gap-4">
-                  {data.permissions.map((item) => (
-                    <Checkbox
-                      key={item.id}
-                      value={item.id}
-                      className={`${
-                        item.categoryId === selectedCategory ||
-                        selectedCategory === 0
-                          ? //   ||
-                            //   searchTerm
-                            //     .toLowerCase()
-                            //     .search(item.name.toLowerCase()) !== -1
-                            "flex"
-                          : "hidden"
+            <div className="flex flex-col gap-4">
+              <Typography.Title level={5}>Assign Permissions</Typography.Title>
+              {/* permission categories */}
+              <div className="flex flex-wrap gap-4 pb-3 border-b ">
+                {data.categories.map((item) => (
+                  <div className="" key={item.id}>
+                    <button
+                      type="button"
+                      className={` capitalize hover:bg-caramel hover:border-caramel focus:bg-caramel active:bg-caramel text-white block rounded-full text-sm cursor-pointer px-2 py-1 hover:text-white ${
+                        item.id === selectedCategory
+                          ? "bg-caramel"
+                          : "bg-transparent border border-slate-400 text-slate-400"
                       }`}
+                      onClick={() => handleCategoryClick(item.id)}
                     >
                       {item.name}
-                    </Checkbox>
-                  ))}
-                </div>
-              </Checkbox.Group>
-            </Form.Item>
-          </div>
+                    </button>
+                  </div>
+                ))}
+              </div>
+              <div className="flex justify-end my-2">
+                <Input
+                  onChange={handleSearch}
+                  className="w-48"
+                  placeholder="Search permissions"
+                  disabled
+                />
+              </div>
+              <Form.Item name="permissionIds" rules={generalValidationRules}>
+                <Checkbox.Group style={{ width: "100%" }}>
+                  <div className="my-6 grid grid-cols-4 gap-4">
+                    {data.permissions.map((item) => (
+                      <Checkbox
+                        key={item.id}
+                        value={item.id}
+                        className={`${
+                          item.categoryId === selectedCategory ||
+                          selectedCategory === 0
+                            ? //   ||
+                              //   searchTerm
+                              //     .toLowerCase()
+                              //     .search(item.name.toLowerCase()) !== -1
+                              "flex"
+                            : "hidden"
+                        }`}
+                      >
+                        {item.name}
+                      </Checkbox>
+                    ))}
+                  </div>
+                </Checkbox.Group>
+              </Form.Item>
+            </div>
 
-          <button className="button" type="submit" disabled={isLoading}>
-            {isLoading ? <BeatLoader /> : "Sign Up"}
-          </button>
-        </Form>
-      )}
+            <button className="button" type="submit" disabled={isLoading}>
+              {isLoading ? <BeatLoader /> : "Sign Up"}
+            </button>
+          </Form>
+        )}
+      </Skeleton>
     </div>
   );
 };
