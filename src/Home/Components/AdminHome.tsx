cbd4dc5f752e0c4c94d3439f86b33a432aeda3a0
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2022-12-01 15:11:17 +0100

    home admin

--- src/Home/Components/AdminHome.tsx
+++ src/Home/Components/AdminHome.tsx
@@ -1,75 +1,101 @@
+import { Tabs } from "antd";
 import { useState } from "react";
+import { useAuthUser } from "react-auth-kit";
 import { Link } from "react-router-dom";
 import DashboardLayout from "../../Layout/DashboardLayout";
-import HomeCard from "../Components/HomeCard";
-import birthDay from "../Assets/Images/birthday.svg";
-import task from "../Assets/Images/task.svg";
-import goals from "../Assets/Images/goals.svg";
-import holiday from "../Assets/Images/holiday.svg";
-import interviews from "../Assets/Images/interviews.svg";
-import timesheets from "../Assets/Images/timesheets.svg";
-import attendance from "../Assets/Images/attendance.svg";
-import files from "../Assets/Images/files.svg";
 import { settingNavItems } from "../../Settings/Data";
-import PendingItem from "../Components/PendingItem";
-import { Dropdown } from "antd";
+import { Celebrations } from "./Celebrations/Celebrations";
+import PendingItem from "./PendingItem";
 import "../style/style.css";
-// import CustomizeDashboardModal from "../Components/CustomizeDashboardModal";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 
 export const AdminHome = () => {
+  const auth = useAuthUser();
+  const authDetails = auth();
+  const user = authDetails?.user;
   const [openId, setOpenId] = useState("");
-  // const [openCustomModal, setOpenCustomModal] = useState(false);
+
   const handlePendingClick = (val: string) => {
     setOpenId((preVal) => (preVal === val ? "" : val));
+  };
+
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
+  const data = {
+    // labels: ["Male", "Female", "Other"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [10, 20, 3],
+        backgroundColor: ["#01966B", "#F97316", "#ccc"],
+        borderColor: ["#01966B", "#F97316", "#ccc"],
+        borderWidth: 1,
+        height: 5,
+      },
+    ],
+
+    maintainAspectRatio: false,
   };
 
   return (
     <DashboardLayout>
       <div className="Container">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-2">
-          <div className="bg-card col-span-2 rounded-xl px-5 py-4">
-            <h5 className="font-semibold text-accent">
-              Welcome Jaleel Habibah 🖐
-            </h5>
-            <div className="flex items-center gap-3 mt-2">
-              <span className="flex items-center gap-2 text-xs text-accent">
-                <i className="ri-calendar-todo-line"></i>
-                <span>Feb 15, 2022</span>
-              </span>
-              <span className="flex items-center gap-2 text-xs text-green-700">
-                <i className="ri-time-line"></i>
-                <span>12:14:59 PM</span>
-              </span>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
-              <div>
-                <h1 className="text-xl md:text-2xl font-black pb-4">
-                  Jaleel Habibah
-                </h1>
-                <ul className="flex flex-col gap-2 text-xs text-accent">
-                  <li>Line manager: Godswill Laser</li>
-                  <li>Email: Gworld@gmail.com</li>
-                  <li>phone: +1-9034-463- 80</li>
-                  <li>location: Houston, TX</li>
-                </ul>
-                <p className="text-xs pt-7 leading-5 text-accent text-justify">
-                  As ultra influential young entrepreneur - Shelby Leimgruber
-                  has succeeded in multiple business facets. From worldwide
-                  travel and modeling to negotiating brand deals with high net
-                  worth clientele. After
-                </p>
+        <div className="flex items-center justify-between mt-2">
+          <h1 className="text-xl md:text-2xl font-black">
+            Welcome {user.fullName}
+          </h1>
+          <button className="transparentButton">Upload Handbook</button>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-5 mt-6">
+          <div className="md:col-span-3">
+            <div className="shadow rounded-lg p-3">
+              <div className="flex items-center justify-between">
+                <h4 className="text-base text-gray-500">Total Employee</h4>
+                <h2 className="font-semibold text-base md:text-lg">
+                  33 People
+                </h2>
               </div>
               <div className="flex justify-center">
-                <img
-                  src="https://res.cloudinary.com/ddvaelej7/image/upload/v1655827312/samples/Image_r0ikln.png"
-                  alt="user"
-                />
+                <div
+                  style={{ height: "200px", width: "200px" }}
+                  className="mt-4 mb-5"
+                >
+                  <Doughnut data={data} />
+                </div>
+              </div>
+              <div className="flex items-center justify-between">
+                <button className="transparentButton flex items-center gap-2">
+                  <div className="rounded-full h-3 w-3 bg-green-700" />
+                  <span>{Math.floor((10 / 33) * 100)}% Male</span>
+                </button>
+                <button className="transparentButton flex items-center gap-2">
+                  <div
+                    className="rounded-full h-3 w-3"
+                    style={{ background: "#F97316" }}
+                  />
+                  <span>{Math.floor((20 / 33) * 100)}% Female</span>
+                </button>
+                <button className="transparentButton flex items-center gap-2">
+                  <div className="rounded-full h-3 w-3 bg-gray-400" />
+                  <span>{Math.floor((3 / 33) * 100)}% Other</span>
+                </button>
               </div>
             </div>
-          
           </div>
-          <div className="bg-card rounded-xl px-5 py-4 text-accent">
+          <div className="md:col-span-1">
+            <div className="border shadow rounded-lg px-3 py-9 flex flex-col gap-3 text-center">
+              <h3 className="text-base text-gray-500">Attendance</h3>
+              <span className="font-medium text-xl">0</span>
+              <hr />
+              <h3 className="text-base text-gray-500">Late</h3>
+              <span className="font-medium text-xl">0</span>
+              <hr />
+              <h3 className="text-base text-gray-500">Absent</h3>
+              <span className="font-medium text-xl">0</span>
+            </div>
+          </div>
+          <div className="bg-card rounded-lg md:col-span-2 p-3 text-accent w-full">
             <h5 className="font-semibold">Pending Setup</h5>
             <div className="flex flex-col gap-5 text-sm mt-4">
               {settingNavItems
@@ -85,7 +111,43 @@ export const AdminHome = () => {
             </div>
           </div>
         </div>
-       
+
+        <div className="grid grid-cols-1 lg:grid-cols-3 mt-7 gap-y-7 gap-x-5">
+          <div className="col-span-2">Graph</div>
+          <div>
+            <div className="bg-mainBg shadow border rounded-lg p-3 mb-6 font-medium">
+              <h5 className="">Assets Held by You</h5>
+              <div className="flex items-center justify-between mt-2">
+                <span>0</span>
+                <Link to="/" className="text-caramel">
+                  View {">"}
+                </Link>
+              </div>
+            </div>
+            <div className="bg-mainBg shadow border rounded-lg p-3">
+              <h3 className="text-base font-medium pb-2">Pending Approval</h3>
+              <hr />
+              <p className="text-center py-5 text-gray-500">
+                You have No Pending <br /> Approval
+              </p>
+            </div>
+          </div>
+          <div className="col-span-2 bg-mainBg shadow border rounded-lg p-3">
+            <h3 className="text-base">Who is out?</h3>
+
+            <Tabs defaultActiveKey="1">
+              <Tabs.TabPane tab="Leave (0)" key="1">
+                <h4>No One is Currently on Leave</h4>
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="Remote Work (0)" key="2">
+                <h4>No Remote Worker</h4>
+              </Tabs.TabPane>
+            </Tabs>
+          </div>
+          <div className="bg-mainBg shadow border rounded-lg p-3">
+            <Celebrations />
+          </div>
+        </div>
       </div>
     </DashboardLayout>
   );
--- src/Home/Pages/Home.tsx
+++ src/Home/Pages/Home.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { AdminHome2 } from "../Components/AdminHome2";
 import { AdminHome } from "../Components/AdminHome";
 import { EmployeeHome } from "../Components/EmployeeHome";
 
@@ -7,8 +6,7 @@ function Home() {
   const [role, setRole] = useState("admin");
   return (
     <div>
-      {role === "admin" && <AdminHome2 />}
-      {/* {role === "admin" && <AdminHome />} */}
+      {role === "admin" && <AdminHome />}
       {role === "employee" && <EmployeeHome />}
     </div>
   );
