50fb1a4f3d1dcfba41f320caf88ca8e6dcab914a
kaasikodes <thisbroisfresh@gmail.com>
2023-03-03 04:07:21 +0100

    created reusable btn component

---
+++ src/GeneralComps/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BeatLoader } from "react-spinners";
+
+interface IProps {
+  isLoading?: boolean;
+  label?: string;
+  type?: "submit" | "reset" | "button";
+}
+
+const Button: React.FunctionComponent<IProps> = ({
+  isLoading = false,
+  label = "submit",
+  type = "button",
+}) => {
+  return (
+    <button className="button capitalize" type={type}>
+      {isLoading ? <BeatLoader color="#fff" /> : label}
+    </button>
+  );
+};
+
+export default Button;
