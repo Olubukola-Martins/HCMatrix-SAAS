e41b3bd74705d5921149035f6535677521c7a624
kaasikodes <thisbroisfresh@gmail.com>
2023-06-09 16:17:37 +0100

    Put an evample of reusable EntityDetailModal

--- src/features/self-service/features/handover-forms/components/HandOverContainer.tsx
+++ src/features/self-service/features/handover-forms/components/HandOverContainer.tsx
@@ -3,21 +3,66 @@ import PageSubHeader from "components/layout/PageSubHeader";
 import { useNavigate } from "react-router-dom";
 import { appRoutes } from "config/router/paths";
 import HandOverTableContainer from "./HandOverTableContainer";
+import { EntityDetailModal } from "components/entity/EntityDetailModal";
 
 export const HandOverContainer = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col gap-6">
-      <PageSubHeader
-        description={`You can now request for time-off to travel for work puposes`}
-        actions={[
+    <>
+      {/* EXAMPLE OF USING REUSABLE entity detail modal */}
+      {/* <EntityDetailModal
+        open={true}
+        handleClose={() => {}}
+        title="Entity"
+        formFields={[
           {
-            name: "Hand Over",
-            handleClick: () => navigate(appRoutes.newHandOverForm),
+            label: "Name",
+            name: "name",
+            render: { value: "James", component: "text" },
+          },
+          {
+            label: "Can User Edit ?",
+            name: "allowEdit",
+            render: { value: true, component: "switch" },
+          },
+          {
+            label: "Date",
+            name: "date",
+            render: { value: "9/10/11", component: "text" },
+          },
+          {
+            label: "Duration",
+            name: "Duration",
+            render: {
+              value: ["9/10/2011", "9/10/2013"],
+              component: "date-range-picker",
+            },
+          },
+          {
+            label: "Documents",
+            name: "Documents",
+            render: {
+              value: [
+                { name: "Car Insurance", url: "/" },
+                { name: "Car Insurance", url: "/" },
+              ],
+              component: "url",
+            },
           },
         ]}
-      />
-      <HandOverTableContainer />
-    </div>
+      /> */}
+      <div className="flex flex-col gap-6">
+        <PageSubHeader
+          description={`You can now request for time-off to travel for work puposes`}
+          actions={[
+            {
+              name: "Hand Over",
+              handleClick: () => navigate(appRoutes.newHandOverForm),
+            },
+          ]}
+        />
+        <HandOverTableContainer />
+      </div>
+    </>
   );
 };
