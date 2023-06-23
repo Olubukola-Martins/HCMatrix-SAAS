d32cfe11565a426f4cdd6a90a9c9c88ac63eb289
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2023-06-23 16:21:52 +0100

    Editable Time Entries

--- src/features/timeAndAttendance/components/settings/AttendanceSettingsIntro.tsx
+++ src/features/timeAndAttendance/components/settings/AttendanceSettingsIntro.tsx
@@ -4,21 +4,23 @@ import { appRoutes } from "config/router/paths";
 
 interface AProps {
   title: string;
+  description: string;
 }
 
-export const AttendanceSettingsIntro = ({ title }: AProps) => {
+export const AttendanceSettingsIntro = ({ title, description }: AProps) => {
   return (
-    <div>
-    <div className="flex items-center justify-between Container">
-      <PageIntro title={title} link={appRoutes.settings} />
+    <div className="Container">
+      <div className="flex items-center justify-between">
+        <PageIntro title={title} link={appRoutes.settings} />
 
-      <div className="flex items-center gap-x-4">
-        <span className="underline text-caramel cursor-pointer hover:text-accent">
-          I will do this later
-        </span>
-        <AppButton label="Next" />
+        <div className="flex items-center gap-x-4">
+          <span className="underline text-caramel cursor-pointer hover:text-accent">
+            I will do this later
+          </span>
+          <AppButton label="Next" />
+        </div>
       </div>
-    </div>
+      <p className="pt-3 text-sm">{description}</p>
     </div>
   );
 };
