f24412ed520bbe44aaa9e80e2aa87a169672a748
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2023-07-01 00:12:50 +0100

    work shedule setup

--- src/features/timeAndAttendance/pages/settings/WorkSchedule.tsx
+++ src/features/timeAndAttendance/pages/settings/WorkSchedule.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { AttendanceSettingsIntro } from "features/timeAndAttendance/components/settings/AttendanceSettingsIntro";
+import { TimeAttendanceSettingsNav } from "features/timeAndAttendance/components/settings/TimeAttendanceSettingsNav";
+import React from "react";
 
 export const WorkSchedule = () => {
   return (
-    <div>WorkSchedule</div>
-  )
-}
+    <>
+      <TimeAttendanceSettingsNav active="Create Work Schedule" />
+      <AttendanceSettingsIntro
+        title={"Create Work Schedule"}
+        description="Plan work by setting your team's work and break time. Manage overtime rules in settings"
+      />
+    </>
+  );
+};
