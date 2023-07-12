8f408594670b895a3f204150cc649070b130958d
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2023-07-12 13:21:34 +0100

    leaning dashboard setup

--- src/features/leaningAndDev/pages/LeaningHome.tsx
+++ src/features/leaningAndDev/pages/LeaningHome.tsx
@@ -1,6 +1,11 @@
-import React from "react";
 import { LeaningNavbar } from "../components/LeaningNavbar";
 import { SimpleCard } from "components/cards/SimpleCard";
+import udemyLogo from "../assets/imgaes/udemyLogo.svg";
+import { Link } from "react-router-dom";
+import { Tabs } from "antd";
+import { LeaningMonthCard } from "../components/LeaningMonthCard";
+import { TrackProgressCard } from "../components/TrackProgressCard";
+import { TrainingCard } from "../components/TrainingCard";
 
 export const LeaningHome = () => {
   return (
@@ -20,7 +25,63 @@ export const LeaningHome = () => {
           <SimpleCard title="Total Employee" highlight="0" />
           <SimpleCard title="Total Number of Trainings" highlight="0" />
           <SimpleCard title="Training Enrollment" highlight="0" />
-          <SimpleCard title="Remote workers" highlight="0" />
+          <div className="border rounded-md p-2 shadow-sm bg-card hover:shadow-md cursor-pointer group">
+            <div
+              className={`rounded-md bg-mainBg shadow px-3 py-6 group-hover:border-b-2 group-hover:border-caramel flex flex-col`}
+            >
+              <>
+                <div>
+                  <img src={udemyLogo} alt="udemy" />
+                </div>
+                <Link
+                  to="/"
+                  className="font-medium text-sm text-right text-caramel underline"
+                >
+                  View Report
+                </Link>
+              </>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 lg:grid-cols-6 gap-5">
+          <div className="col-span-4">
+            <LeaningMonthCard />
+          </div>
+          <div className="lg:col-span-2 col-span-6">
+            <TrackProgressCard />
+          </div>
+
+          <div className="col-span-4 bg-mainBg shadow border rounded-lg p-3">
+            <h3 className="text-base">Gamification?</h3>
+
+            <Tabs
+              defaultActiveKey="1"
+              items={[
+                {
+                  key: "1",
+                  label: "Badges",
+                  children: (
+                    <>
+                      <h4>No One is Currently on Leave</h4>
+                    </>
+                  ),
+                },
+                {
+                  key: "2",
+                  label: "Points",
+                  children: (
+                    <>
+                      <h4>No Point</h4>
+                    </>
+                  ),
+                },
+              ]}
+            />
+          </div>
+          <div className="lg:col-span-2 col-span-6">
+            <TrainingCard />
+          </div>
         </div>
       </div>
     </>
