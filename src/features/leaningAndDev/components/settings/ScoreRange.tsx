7bb7bdbf79e73c413cd537413442d3c7e3ba5b02
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2023-07-12 11:19:14 +0100

    score range table

--- src/features/leaningAndDev/components/settings/ScoreRange.tsx
+++ src/features/leaningAndDev/components/settings/ScoreRange.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export const ScoreRange = () => {
   return (
-    <div>ScoreRange</div>
-  )
-}
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between items-center border-b border-slate-400 pb-2">
+        <span>Score Title</span>
+        <span>Score Range</span>
+      </div>
+      <div className="flex justify-between items-center border-b border-slate-400 pb-2">
+        <button className="transparentButton w-32">Excellent</button>
+        <button className="transparentButton w-20">100%</button>
+      </div>
+      <div className="flex justify-between items-center border-b border-slate-400 pb-2">
+        <button className="transparentButton w-32">Good</button>
+        <button className="transparentButton w-20">80%</button>
+      </div>
+      <div className="flex justify-between items-center border-b border-slate-400 pb-2">
+        <button className="transparentButton w-32">Average</button>
+        <button className="transparentButton w-20">60%</button>
+      </div>
+      <div className="flex justify-between items-center border-b border-slate-400 pb-2">
+        <button className="transparentButton w-32">Poor</button>
+        <button className="transparentButton w-20">50%</button>
+      </div>
+      <div className="flex justify-between items-center border-b border-slate-400 pb-2">
+        <button className="transparentButton  w-32">Unsatisfactory</button>
+        <button className="transparentButton w-20">40%</button>
+      </div>
+    </div>
+  );
+};
