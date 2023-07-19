55c0486361be192a294117cfc57d8475ae24ab32
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2023-07-19 13:51:04 +0100

    update training

--- src/features/leaningAndDev/pages/TrainingDetails.tsx
+++ src/features/leaningAndDev/pages/TrainingDetails.tsx
@@ -1,5 +1,52 @@
-import React from "react";
+import { PageIntro } from "components/layout/PageIntro";
+import { appRoutes } from "config/router/paths";
+import { LeaningNavbar } from "../components/LeaningNavbar";
+import { Tabs } from "antd";
+import { Overview } from "../components/trainings/Overview";
+import { UpdateTraining } from "../components/trainings/UpdateTraining";
 
 export const TrainingDetails = () => {
-  return <div>TrainingDetails</div>;
+  return (
+    <>
+      <LeaningNavbar active="training" />
+
+      <div className="Container">
+        <PageIntro title="Training Details" link={appRoutes.training} />
+        <Tabs
+          defaultActiveKey="1"
+          className="mt-2"
+          items={[
+            {
+              key: "1",
+              label: "Overview",
+              children: (
+                <>
+                  <Overview />
+                </>
+              ),
+            },
+            {
+              key: "2",
+              label: "Training Content",
+              children: <>Training Content</>,
+            },
+            {
+              key: "3",
+              label: "Users",
+              children: <>Users</>,
+            },
+            {
+              key: "4",
+              label: "Update Training",
+              children: (
+                <>
+                  <UpdateTraining />
+                </>
+              ),
+            },
+          ]}
+        />
+      </div>
+    </>
+  );
 };
