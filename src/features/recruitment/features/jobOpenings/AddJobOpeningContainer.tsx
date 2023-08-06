5d98e6dc45bd706fcb12f91d154c1968bb1a4219
Olubukola <19bukola97@gmail.com>
2023-08-06 22:30:25 +0100

    form structure and stepper complete

--- src/features/recruitment/features/jobOpenings/AddJobOpeningContainer.tsx
+++ src/features/recruitment/features/jobOpenings/AddJobOpeningContainer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Steps } from "antd";
+import { Steps, Form, Input, Select, Button, Checkbox } from "antd";
 import "antd/dist/antd.min.css";
 
 export const AddJobOpeningContainer = () => {
   const { Step } = Steps;
+  const { TextArea } = Input;
 
   return (
     <>
@@ -15,10 +16,10 @@ export const AddJobOpeningContainer = () => {
       </div> */}
 
       {/* ADD JOB OPENING */}
-      <div className="w-fit flex gap-2">
-        <a href="">
+      <div className="w-[82.1vw] flex gap-2 mx-auto">
+        <a href="" className="flex flex-col items-center">
           <svg
-            className="m-auto"
+            className="my-auto"
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
@@ -39,11 +40,206 @@ export const AddJobOpeningContainer = () => {
       </div>
 
       {/* STEPPER */}
-      <Steps current={1} labelPlacement="vertical">
-        <Step description="Job details" />
-        <Step description="Application Questions" />
-        <Step description="Additional Questions" />
-      </Steps>
+      <div className="w-[82.1vw] h-36 mx-auto my-6 rounded-2xl shadow shadow-[rgba(0, 0, 0, 0.08)] ">
+        <Steps current={1} labelPlacement="vertical" className="py-6 h-auto">
+          <Step description="Job details" />
+          <Step description="Application Questions" />
+          <Step description="Additional Questions" />
+        </Steps>
+      </div>
+
+      {/* FORM */}
+      <section className="">
+        <Form
+          layout="vertical"
+          // labelCol={{ span: 8 }}
+          // wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+        >
+          <div>
+            <Form.Item label="Job Title" name="Job Title">
+              <Input placeholder="Mobile Developer"></Input>
+            </Form.Item>
+          </div>
+
+          <div>
+            <Form.Item label="Department" name="Department">
+              <Select
+                defaultValue="Application Development"
+                options={[
+                  {
+                    value: "Application Development",
+                    label: "Application Development",
+                  },
+                  {
+                    value: "CSI",
+                    label: "CSI",
+                  },
+                  {
+                    value: "Sales",
+                    label: "Sales",
+                  },
+                  {
+                    value: "Marketing",
+                    label: "Marketing",
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="Hiring Lead" name="Hiring Lead">
+              <Select
+                defaultValue="Basil Ikpe (Product Manager)"
+                options={[
+                  {
+                    value: "Basil Ikpe (Product Manager)",
+                    label: "Basil Ikpe (Product Manager)",
+                  },
+                  {
+                    value: "Esther Adiele (HR Manager)",
+                    label: "Esther Adiele (HR Manager)",
+                  },
+                ]}
+              />
+            </Form.Item>
+          </div>
+
+          <div>
+            <Form.Item label="Employment Type" name="Employment Type">
+              <Select
+                defaultValue="Full-Time"
+                options={[
+                  {
+                    value: "Contract",
+                    label: "Contract",
+                  },
+                  {
+                    value: "Full-Time",
+                    label: "Full-Time",
+                  },
+                  {
+                    value: "Intern",
+                    label: "Intern",
+                  },
+                  {
+                    value: "Part-Time",
+                    label: "Part-Time",
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="Minimum Experience" name="Minimum Experience">
+              <Select
+                defaultValue="Entry Level"
+                options={[
+                  {
+                    value: "Entry Level",
+                    label: "Entry Level",
+                  },
+                  {
+                    value: "Mid-Level",
+                    label: "Mid-Level",
+                  },
+                  {
+                    value: "Experienced",
+                    label: "Experienced",
+                  },
+                  {
+                    value: "Manager/Supervisor",
+                    label: "Manager/Supervisor",
+                  },
+                  {
+                    value: "Senior Manager/Supervisor",
+                    label: "Senior Manager/Supervisor",
+                  },
+                  {
+                    value: "Executive",
+                    label: "Executive",
+                  },
+                  {
+                    value: "Senior Executive",
+                    label: "Senior Executive",
+                  },
+                ]}
+              />
+            </Form.Item>
+          </div>
+
+          <div>
+            <Form.Item label="Job Location" name="Job Location">
+              <Select
+                defaultValue="Onsite"
+                options={[
+                  {
+                    value: "Onsite",
+                    label: "Onsite",
+                  },
+                  {
+                    value: "Remote",
+                    label: "Remote",
+                  },
+                  {
+                    value: "Hybrid",
+                    label: "Hybrid",
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item label="Location" name="Location">
+              <Input placeholder="e.g Charles Okorocha Lekki Phase 1"></Input>
+            </Form.Item>
+          </div>
+
+          <div>
+            <Form.Item label="Compensation" name="Compensation">
+              <Input placeholder="e.g  NGN 200,000 Monthly"></Input>
+              <Form.Item name="Not Specified" >
+                <Checkbox>Not Specified</Checkbox>
+              </Form.Item>
+            </Form.Item>
+            <Form.Item label="" name="">
+              <Input placeholder="Add pay range e.g NGN 200,000 to NGN 500,0000"></Input>
+            </Form.Item>
+          </div>
+
+          <div>
+            <Form.Item label="Job Template" name="Job Template">
+              <Select
+                defaultValue="Search job template"
+                options={[
+                  {
+                    value: "template 1",
+                    label: "template 1",
+                  },
+                  {
+                    value: "template 2",
+                    label: "template 2",
+                  },
+                ]}
+              />
+            </Form.Item>
+          </div>
+
+          <div>
+            <Form.Item label="Job Description" name="Job Description">
+              <TextArea
+                placeholder="Input description"
+                autoSize={{ minRows: 3 }}
+              />
+            </Form.Item>
+          </div>
+
+          <div>
+            <Form.Item>
+              <Button type="text">Cancel</Button>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Next
+              </Button>
+            </Form.Item>
+          </div>
+        </Form>
+      </section>
     </>
   );
 };
