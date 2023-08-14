f5a9f3d19440d3f4ee3fbe3e25a45ddedb27de34
Godswill Omenuko <omenukoonyekachi690@gmail.com>
2023-08-14 16:44:27 +0100

    added level

--- src/features/leaningAndDev/pages/settings/GamificationSettings.tsx
+++ src/features/leaningAndDev/pages/settings/GamificationSettings.tsx
@@ -59,6 +59,7 @@ export const GamificationSettings = () => {
                     options={[
                       { value: 1, label: "Level 1" },
                       { value: 2, label: "Level 2" },
+                      { value: 3, label: "Level 3" },
                     ]}
                   />
                 </Form.Item>
