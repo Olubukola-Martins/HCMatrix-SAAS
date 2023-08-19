f60b23265a958fffdfa2f32aaf5920bde227432c
kaasikodes <thisbroisfresh@gmail.com>
2023-08-19 03:12:48 +0100

    feat(payroll): adds ability to approve/reject payroll

--- src/features/payroll/components/payrollReviews/SinglePayrollReview.tsx
+++ src/features/payroll/components/payrollReviews/SinglePayrollReview.tsx
@@ -3,14 +3,20 @@ import React, { useState } from "react";
 import { EmployeePayrollUpdatesContainer } from "../employeePayrollUpdates/EmployeePayrollUpdatesContainer";
 import PageSubHeader from "components/layout/PageSubHeader";
 import { MoreOutlined } from "@ant-design/icons";
-import { Button, Dropdown, Menu } from "antd";
+import { Button, Dropdown, Menu, Skeleton } from "antd";
 import PayrollBreakdown from "./PayrollBreakdown";
+import { useApproveORReject } from "hooks/useApproveORReject";
+import { QUERY_KEY_FOR_PAYROLLS_BY_SCHEME } from "features/payroll/hooks/payroll/useGetAllPayrollsByScheme";
+import { useQueryClient } from "react-query";
+import { useFetchApprovalRequests } from "features/core/workflows/hooks/useFetchApprovalRequests";
 
 interface IProps {
   payroll: TSinglePayroll;
 }
 const SinglePayrollReview: React.FC<IProps> = ({ payroll }) => {
   type TAction = "view-summary" | "compare";
+  const queryClient = useQueryClient();
+
   const [action, setAction] = useState<TAction>();
   const handleAction = (props: { action: TAction }) => {
     const { action } = props;
@@ -19,6 +25,27 @@ const SinglePayrollReview: React.FC<IProps> = ({ payroll }) => {
   const clearAction = () => {
     setAction(undefined);
   };
+
+  const { data: payrollApprRequests, isFetching: isFetchingApprRequests } =
+    useFetchApprovalRequests({
+      pagination: {
+        limit: 200,
+        offset: 0,
+      },
+      type: "payroll",
+    });
+  const payrollRequestItem = payrollApprRequests?.data.find(
+    (item) => item.payroll?.id === payroll.id
+  );
+
+  const { confirmApprovalAction } = useApproveORReject({
+    handleSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEY_FOR_PAYROLLS_BY_SCHEME],
+        // exact: true,
+      });
+    },
+  });
   return (
     <>
       <PayrollBreakdown
@@ -26,49 +53,76 @@ const SinglePayrollReview: React.FC<IProps> = ({ payroll }) => {
         handleClose={clearAction}
         payroll={payroll}
       />
-      <div className="flex flex-col gap-6">
-        <PageSubHeader
-          description={`Review ${payroll.name} payroll being processed`}
-          hideBackground
-          actions={[
-            { name: "Approve", handleClick: () => {} },
-            {
-              name: "Reject",
-              handleClick: () => {},
-              btnVariant: "style-with-class",
-              additionalClassNames: ["neutralButton"],
-            },
-          ]}
-          comps={[
-            <Dropdown
-              overlay={
-                <Menu
-                  items={[
+      <Skeleton
+        active
+        paragraph={{ rows: 20 }}
+        loading={isFetchingApprRequests}
+      >
+        <div className="flex flex-col gap-6">
+          <PageSubHeader
+            description={`Review ${payroll.name} payroll being processed`}
+            hideBackground
+            actions={
+              payrollRequestItem
+                ? [
                     {
-                      key: "view",
-                      label: "View Total Summary",
-                      onClick: () => handleAction({ action: "view-summary" }),
+                      name: "Approve",
+                      handleClick: () =>
+                        confirmApprovalAction({
+                          approvalStageId: payrollRequestItem?.id,
+                          status: "approved",
+                          workflowType: !!payrollRequestItem?.basicStageId
+                            ? "basic"
+                            : "advanced",
+                        }),
                     },
                     {
-                      key: "compare",
-                      label: "Compare",
-                      onClick: () => handleAction({ action: "compare" }),
+                      name: "Reject",
+                      handleClick: () =>
+                        confirmApprovalAction({
+                          approvalStageId: payrollRequestItem?.id,
+                          status: "rejected",
+                          workflowType: !!payrollRequestItem?.basicStageId
+                            ? "basic"
+                            : "advanced",
+                        }),
+                      btnVariant: "style-with-class",
+                      additionalClassNames: ["neutralButton"],
                     },
-                  ]}
-                />
-              }
-            >
-              <Button type="text" icon={<MoreOutlined />} />
-            </Dropdown>,
-          ]}
-        />
-        <EmployeePayrollUpdatesContainer
-          expatriate={false}
-          payrollId={payroll?.id}
-          allowMultipleSelect={false}
-          allowedEmployeeActions={["view-details"]}
-        />
-      </div>
+                  ]
+                : undefined
+            }
+            comps={[
+              <Dropdown
+                overlay={
+                  <Menu
+                    items={[
+                      {
+                        key: "view",
+                        label: "View Total Summary",
+                        onClick: () => handleAction({ action: "view-summary" }),
+                      },
+                      {
+                        key: "compare",
+                        label: "Compare",
+                        onClick: () => handleAction({ action: "compare" }),
+                      },
+                    ]}
+                  />
+                }
+              >
+                <Button type="text" icon={<MoreOutlined />} />
+              </Dropdown>,
+            ]}
+          />
+          <EmployeePayrollUpdatesContainer
+            expatriate={false}
+            payrollId={payroll?.id}
+            allowMultipleSelect={false}
+            allowedEmployeeActions={["view-details"]}
+          />
+        </div>
+      </Skeleton>
     </>
   );
 };
