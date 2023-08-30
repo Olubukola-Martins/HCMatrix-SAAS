1ca2413f946240a4abd7b033b8d1d96cbf767548
Olubukola <19bukola97@gmail.com>
2023-08-30 14:59:03 +0100

    style: date range in applications width adjusted to full

--- src/features/recruitment/features/applications/ApplicationsList.tsx
+++ src/features/recruitment/features/applications/ApplicationsList.tsx
@@ -394,7 +394,7 @@ const ApplicationsList = () => {
             />
           </Form.Item>
           <Form.Item label="Date Range" name="dateRange">
-            <RangePicker />
+            <RangePicker className="w-full"/>
           </Form.Item>
           <Form.Item name="hiringLead" label="Hiring Lead">
             <Select
