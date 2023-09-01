9b0deb95003f782c510a2bac75c2433ee1d0a82b
kaasikodes <thisbroisfresh@gmail.com>
2023-09-01 07:56:58 +0100

    feat(approval): added verification to 2fa setup

--- src/components/twoFactorAuth/SetUp2FA.tsx
+++ src/components/twoFactorAuth/SetUp2FA.tsx
@@ -6,6 +6,9 @@ import { useSetup2FA } from "features/core/employees/hooks/twoFactorAuth/useSetu
 import { QUERY_KEY_FOR_EMPLOYEE_2FA } from "features/core/employees/hooks/twoFactorAuth/useGetEmployee2FA";
 import ConfirmationModal from "components/modals/ConfirmationModal";
 import DisplayModal from "components/modals/DisplayModal";
+import { Input } from "antd";
+import { AppButton } from "components/button/AppButton";
+import { useVerify2FA } from "features/core/employees/hooks/twoFactorAuth/verify/useVerify2FA";
 
 export const Setup2FA: React.FC<IModalProps> = ({ open, handleClose }) => {
   type TAction = "display-qrcode";
@@ -14,7 +17,41 @@ export const Setup2FA: React.FC<IModalProps> = ({ open, handleClose }) => {
   const queryClient = useQueryClient();
 
   const { mutate, isLoading } = useSetup2FA();
+  const { mutate: verify, isLoading: isLoadingVerify } = useVerify2FA();
+  const [token, setToken] = useState<string>();
 
+  const handleVerify = () => {
+    if (!token) return;
+    verify(
+      {
+        token,
+      },
+      {
+        onError: (err: any) => {
+          openNotification({
+            state: "error",
+            title: "Error Occurred",
+            description:
+              err?.response.data.message ?? err?.response.data.error.message,
+          });
+        },
+        onSuccess: (res) => {
+          openNotification({
+            state: "success",
+
+            title: "Success",
+            description: res.data.message,
+            // duration: 0.4,
+          });
+          onClose();
+          queryClient.invalidateQueries({
+            queryKey: [QUERY_KEY_FOR_EMPLOYEE_2FA],
+            // exact: true,
+          });
+        },
+      }
+    );
+  };
   const handleSubmit = () => {
     mutate(
       {},
@@ -51,6 +88,7 @@ export const Setup2FA: React.FC<IModalProps> = ({ open, handleClose }) => {
     handleClose();
     setAction(undefined);
     setImage(undefined);
+    setToken("");
   };
   return (
     <>
@@ -59,7 +97,23 @@ export const Setup2FA: React.FC<IModalProps> = ({ open, handleClose }) => {
         description={`You can use any authenticator of your choice to scan QR code`}
         handleClose={onClose}
         open={action === "display-qrcode"}
-        component={<img src={image} alt={"qrcode"} />}
+        component={
+          <div className="flex flex-col gap-2">
+            <img src={image} alt={"qrcode"} />
+            <div className="flex gap-4">
+              <Input
+                value={token}
+                onChange={(e) => setToken(e.target.value)}
+                placeholder="Enter Code from authenticator"
+              />
+              <AppButton
+                label="Verify"
+                isLoading={isLoadingVerify}
+                handleClick={handleVerify}
+              />
+            </div>
+          </div>
+        }
       />
       <ConfirmationModal
         title={`Set Up 2 Factor Authentication`}
