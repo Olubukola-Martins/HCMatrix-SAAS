f69ab1eee67b543976594bc6dc4109c42e06c62b
kaasikodes <thisbroisfresh@gmail.com>
2023-11-01 12:50:17 +0100

    chore(app-tooltip): refactors app tooltip to use card bg

--- src/components/tooltip/AppTooltip.tsx
+++ src/components/tooltip/AppTooltip.tsx
@@ -4,15 +4,24 @@ import React, { ReactNode } from "react";
 
 const AppTooltip: React.FC<{
   children: ReactNode;
-  tooltipProps: { title: string; showArrow?: boolean };
+  tooltipProps: { title: React.ReactNode; showArrow?: boolean };
 }> = ({ children, tooltipProps }) => {
-  const { title, showArrow = false } = tooltipProps;
+  const { title, showArrow = true } = tooltipProps;
   return (
     <Themes isBg={false}>
       <div className="flex gap-2 items-start">
         <div>{children}</div>
-        <Tooltip showArrow={showArrow} title={title}>
-          <i className="ri-information-fill text-sm pt-0.5" />
+        <Tooltip
+          showArrow={showArrow}
+          title={title}
+          trigger={["hover"]}
+          overlayInnerStyle={{
+            background: "var(--card)",
+            color: "black",
+            padding: "10px",
+          }}
+        >
+          <i className="ri-information-fill text-sm pt-0.5 text-caramel" />
         </Tooltip>
       </div>
     </Themes>
