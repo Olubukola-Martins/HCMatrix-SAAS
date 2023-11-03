4f24182b5014a3dad5830657fa581046bf133ee9
kaasikodes <thisbroisfresh@gmail.com>
2023-11-03 12:52:17 +0100

    chore: adds todo to notification

--- src/features/notifications/components/ViewNotification.tsx
+++ src/features/notifications/components/ViewNotification.tsx
@@ -7,6 +7,7 @@ interface IProps extends IModalProps {
   content?: TNotification["content"];
 }
 const ViewNotification: React.FC<IProps> = ({ handleClose, open, content }) => {
+  // TODO: Update to account for all the possible content data, also update type content in TNotification e.g Task
   return (
     <ViewApprovalRequest
       handleClose={handleClose}
