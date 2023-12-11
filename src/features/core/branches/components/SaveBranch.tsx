7a2ceeaeb4301ca67c28f85793ea8718e568e131
kaasikodes <thisbroisfresh@gmail.com>
2023-12-11 17:20:22 +0100

    refactor(branch): now uses address input

--- src/features/core/branches/components/SaveBranch.tsx
+++ src/features/core/branches/components/SaveBranch.tsx
@@ -6,11 +6,10 @@ import {
   generalValidationRules,
   textInputValidationRules,
 } from "utils/formHelpers/validation";
-import { SelectCountry } from "components/selectEntity/SelectCountry";
-import { SelectState } from "components/selectEntity/SelectState";
+
 import { TBranch, TCreateBranchProps } from "../types";
-import { SelectLGA } from "components/selectEntity/SelectLGA";
-import { TIME_ZONES } from "constants/timeZones";
+
+import { FormAddressInput } from "components/generalFormInputs/FormAddressInput";
 
 interface IProps extends IModalProps {
   branch?: TBranch;
@@ -34,57 +33,32 @@ export const SaveBranch: React.FC<IProps> = ({
   loading,
 }) => {
   const [form] = Form.useForm<TCreateBranchProps>();
-  const [errors, setErrors] = useState<{
-    [key: string]: string | undefined;
-  }>({});
-  const [countryId, setCountryId] = useState<number>();
-  const [stateId, setStateId] = useState<number>();
-  const [lgaId, setLgaId] = useState<number>();
-  const [doesStateHaveLGAS, setDoesStateHaveLGAS] = useState(false);
 
   const handleSubmit = (data: TCreateBranchProps) => {
-    setErrors((prev) => ({
-      ...prev,
-      country:
-        typeof countryId === "undefined" ? "Please fill country!" : undefined,
-      state:
-        typeof stateId === "undefined" && typeof countryId === "number"
-          ? "Please fill state!"
-          : undefined,
-      lga:
-        typeof lgaId === "undefined" &&
-        typeof countryId === "number" &&
-        typeof stateId === "number" &&
-        doesStateHaveLGAS
-          ? "Please fill LGA!"
-          : undefined,
-    }));
-
-    if (!countryId) return;
-    if (!stateId) return;
-    if (!lgaId && doesStateHaveLGAS) return;
-
     onSubmit?.fn({
       ...data,
-      address: { ...data.address, countryId, stateId, lgaId },
+      address: { ...data.address, lgaId: data?.address?.lgaId ?? undefined },
     });
   };
 
   useEffect(() => {
     if (onSubmit?.isSuccess) {
       form.resetFields();
-      setCountryId(undefined);
-      setStateId(undefined);
-      setLgaId(undefined);
     }
   }, [form, onSubmit?.isSuccess]);
 
   useEffect(() => {
     if (branch) {
-      form.setFieldsValue({ ...branch });
-      setCountryId(branch?.address.countryId);
-      setStateId(branch?.address.stateId);
-      setLgaId(branch?.address.lgaId);
+      form.setFieldsValue({
+        ...branch,
+        address: {
+          timezone: branch?.address.timezone,
+          streetAddress: branch?.address.streetAddress,
+          countryId: branch?.address.countryId,
+          stateId: branch?.address.stateId,
+          lgaId: branch?.address.lgaId,
+        },
+      });
     }
   }, [branch, form]);
 
@@ -118,80 +92,7 @@ export const SaveBranch: React.FC<IProps> = ({
           {/* TODO: Create a reusable address form component */}
           <>
             {" "}
-            <Form.Item name="address" label="Address">
-              <Input.Group className="grid grid-cols-2 gap-4">
-                <div className="col-span-2">
-                  <Form.Item
-                    noStyle
-                    rules={textInputValidationRules}
-                    name={["address", "streetAddress"]}
-                  >
-                    <Input.TextArea placeholder="Street Address" />
-                  </Form.Item>
-                </div>
-                <Form.Item noStyle>
-                  <SelectCountry
-                    handleSelect={(val) => {
-                      setCountryId(val);
-                      setErrors((prev) => ({ ...prev, country: undefined }));
-                    }}
-                    onClear={() => {
-                      setCountryId(undefined);
-                      setStateId(undefined);
-                      setLgaId(undefined);
-                    }}
-                    value={countryId}
-                  />
-                </Form.Item>
-                <Form.Item noStyle>
-                  <SelectState
-                    countryId={countryId}
-                    handleSelect={(val) => {
-                      setStateId(val);
-                      setErrors((prev) => ({ ...prev, state: undefined }));
-                    }}
-                    onClear={() => {
-                      setStateId(undefined);
-                      setLgaId(undefined);
-                    }}
-                    value={stateId}
-                  />
-                </Form.Item>
-                <Form.Item noStyle>
-                  <SelectLGA
-                    stateId={stateId}
-                    handleSelect={(val) => {
-                      setLgaId(val);
-                      setErrors((prev) => ({ ...prev, lga: undefined }));
-                    }}
-                    onClear={() => {
-                      setLgaId(undefined);
-                    }}
-                    onFetchSuccess={(dataIsEmpty) =>
-                      setDoesStateHaveLGAS(!dataIsEmpty)
-                    }
-                    value={lgaId}
-                  />
-                </Form.Item>
-                <Form.Item
-                  noStyle
-                  name={["address", "timezone"]}
-                  rules={generalValidationRules}
-                >
-                  <Select options={TIME_ZONES} placeholder="Select Timezone" />
-                </Form.Item>
-              </Input.Group>
-            </Form.Item>
-            {/* errors */}
-            <div className="flex flex-col gap-1">
-              {Object.values(errors)
-                .filter((item) => typeof item != "undefined")
-                .map((item, i) => (
-                  <span key={i} className="text-sm text-red-500">
-                    {item}
-                  </span>
-                ))}
-            </div>
+            <FormAddressInput Form={Form} form={form} />
           </>
 
           {action !== "view" ? (
