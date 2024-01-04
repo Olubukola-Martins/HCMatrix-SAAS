f72e55a79028b53f49c87d92343fef0d7fb8428a
kaasikodes <thisbroisfresh@gmail.com>
2024-01-04 06:50:16 +0100

    chore: updates images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,10 @@
+import employeeManagementSvg from "./employee-management.svg";
 import deleteEntitySvg from "./deleteEntity.svg";
 import confirmActionSvg from "./confirmAction.svg";
 import hcMatrixWatermarkSvg from "./hcMatrixWatermark.svg";
-export { deleteEntitySvg, confirmActionSvg, hcMatrixWatermarkSvg };
+export {
+  deleteEntitySvg,
+  confirmActionSvg,
+  hcMatrixWatermarkSvg,
+  employeeManagementSvg,
+};
