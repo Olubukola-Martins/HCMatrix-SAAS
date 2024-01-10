79525c98e88e5be240b372fc664549e26eda7cfe
kaasikodes <thisbroisfresh@gmail.com>
2024-01-10 06:27:04 +0100

    chore: adds ability to cancel company training session from calender

--- src/features/billing/components/trainingSessions/CompanyTrainingSessionCalender.tsx
+++ src/features/billing/components/trainingSessions/CompanyTrainingSessionCalender.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
+import moment, { Moment } from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import ErrorBoundary from "components/errorHandlers/ErrorBoundary";
 import Skeleton from "antd/lib/skeleton/Skeleton";
 import { ErrorWrapper } from "components/errorHandlers/ErrorWrapper";
 import "../../styles/big-calender-override.css";
+import { CancelCompanyTrainingSession } from "./CancelCompanyTrainingSession";
 
 const localizer = momentLocalizer(moment);
 const CompanyTrainingSessionCalender = () => {
   // TODO: Get event list from api
+  const [action, setAction] = React.useState<"cancel-training-session">();
+  const [selectedEvent, setSelectedEvent] = React.useState<{
+    title: string;
+    start: Moment;
+    end: Moment;
+  }>();
   return (
     <ErrorBoundary>
       <Skeleton loading={false} active paragraph={{ rows: 45 }}>
@@ -18,6 +25,11 @@ const CompanyTrainingSessionCalender = () => {
           message={`error?.response?.data?.message ??
               error?.response?.data?.error?.message`}
         >
+          <CancelCompanyTrainingSession
+            open={action === "cancel-training-session"}
+            handleClose={() => setAction(undefined)}
+            event={selectedEvent}
+          />
           <Calendar
             localizer={localizer}
             events={[
@@ -37,6 +49,10 @@ const CompanyTrainingSessionCalender = () => {
                 title: "Training Session 3",
               },
             ]}
+            onSelectEvent={(event) => {
+              setSelectedEvent(event);
+              setAction("cancel-training-session");
+            }}
             style={{ height: 500 }}
           />
         </ErrorWrapper>
--- src/features/billing/components/trainingSessions/CompanyTrainingSessionContainer.tsx
+++ src/features/billing/components/trainingSessions/CompanyTrainingSessionContainer.tsx
@@ -4,7 +4,10 @@ import CompanyTrainingSessionCalender from "./CompanyTrainingSessionCalender";
 const CompanyTrainingSessionContainer = () => {
   return (
     <div className="flex flex-col gap-4">
-      <p className=" font-light">Schedule your training session.</p>
+      <p className=" font-light">
+        Schedule your training session and cancel them by clicking on the event
+        on the calender.
+      </p>
       <div>
         <CompanyTrainingSessionCalender />
       </div>
