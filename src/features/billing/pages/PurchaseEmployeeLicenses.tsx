714079c645e0534f6d4a904ecb577ca43d78ebaa
kaasikodes <thisbroisfresh@gmail.com>
2024-01-10 10:29:12 +0100

    updates

--- src/features/billing/pages/PurchaseEmployeeLicenses.tsx
+++ src/features/billing/pages/PurchaseEmployeeLicenses.tsx
@@ -1,13 +1,11 @@
 import { PageIntro } from "components/layout/PageIntro";
 import { appRoutes } from "config/router/paths";
-import React, { useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { useGetCompanyActiveSubscription } from "../hooks/company/useGetCompanyActiveSubscription";
 import { ErrorBoundary } from "components/errorHandlers/ErrorBoundary";
 import { Skeleton } from "antd";
 import { ErrorWrapper } from "components/errorHandlers/ErrorWrapper";
 import UserSelectionContainer from "../components/subscription/userSelection/UserSelectionContainer";
-import { CancelSubscription } from "../components/subscription/CancelSubscription";
 
 const PurchaseEmployeeLicenses = () => {
   const navigate = useNavigate();
@@ -18,16 +16,12 @@ const PurchaseEmployeeLicenses = () => {
     error,
   } = useGetCompanyActiveSubscription();
   const unableToPurchaseUserLicense = !subscription || !subscription?.isActive;
-  const [action, setAction] = useState<"cancel-subscription">();
   return (
     <>
       {unableToPurchaseUserLicense && (
         <Navigate to={appRoutes.billingSubscription} replace={true} />
       )}
-      <CancelSubscription
-        handleClose={() => setAction(undefined)}
-        open={action === "cancel-subscription"}
-      />
+
       <ErrorBoundary>
         <Skeleton loading={isLoading} active paragraph={{ rows: 45 }}>
           <ErrorWrapper
@@ -45,11 +39,6 @@ const PurchaseEmployeeLicenses = () => {
                   {
                     name: "Billing Summary",
                     handleClick: () => navigate(appRoutes.billingSummary),
-                  },
-                  {
-                    name: "Cancel Subscription",
-                    handleClick: () => setAction("cancel-subscription"),
-                    btnVariant: "transparent",
                   },
                 ]}
               />
