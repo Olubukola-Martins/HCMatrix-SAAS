f370f39b01d7326131a6e841401d65697337676e
kaasikodes <thisbroisfresh@gmail.com>
2024-01-10 07:47:43 +0100

    chore: adds hook 4 upgrading extrastorage

---
+++ src/features/billing/hooks/addOns/extraStorage/useUpgradeExtraStorage.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { MICROSERVICE_ENDPOINTS } from "config/enviroment";
+import { useApiAuth } from "hooks/useApiAuth";
+import { useMutation } from "react-query";
+import { ICurrentCompany } from "types";
+import { TSubscriptionPriceType } from "features/billing/types/priceType";
+
+type TData = {
+  extraStorageId: number;
+  priceType: TSubscriptionPriceType;
+};
+const createData = async (props: { data: TData; auth: ICurrentCompany }) => {
+  const url = `${MICROSERVICE_ENDPOINTS.UTILITY}/subscription/add-ons/extra-storage`;
+  const config = {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${props.auth.token}`,
+      "x-company-id": props.auth.companyId,
+    },
+  };
+
+  const data: TData = {
+    ...props.data,
+  };
+
+  const response = await axios.post(url, data, config);
+  return response;
+};
+export const useUpgradeExtraStorage = () => {
+  const { token, companyId } = useApiAuth();
+  return useMutation((props: TData) =>
+    createData({ data: props, auth: { token, companyId } })
+  );
+};
