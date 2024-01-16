4cadbdb8810ec92771c6930dfd93d2c0463b6692
kaasikodes <thisbroisfresh@gmail.com>
2024-01-16 09:02:15 +0100

    chore: adds back btn to files

--- src/features/self-service/features/documents/pages/DocumentsPage.tsx
+++ src/features/self-service/features/documents/pages/DocumentsPage.tsx
@@ -1,13 +1,14 @@
 import SelfServiceSubNav from "features/self-service/components/SelfServiceSubNav";
 import DocumentsContainer from "../components/DocumentsContainer";
 import { PageIntro } from "components/layout/PageIntro";
+import { appRoutes } from "config/router/paths";
 
 const DocumentsPage = () => {
   return (
     <>
       <SelfServiceSubNav />
       <div className="Container ">
-        <PageIntro title="Files" />
+        <PageIntro title="Files" link={appRoutes.settings} />
         <DocumentsContainer />
       </div>
     </>
