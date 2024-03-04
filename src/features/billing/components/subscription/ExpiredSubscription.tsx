0fdd7c2b9989ac4a5a60771a3df362dc8388dead
kaasikodes <thisbroisfresh@gmail.com>
2024-03-04 14:41:48 +0100

    chore: informs owner of expired subscription via modal n page

---
+++ src/features/billing/components/subscription/ExpiredSubscription.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { IModalProps } from "types";
+import ConfirmationModal from "components/modals/ConfirmationModal";
+import useMostRecentApiAuth from "hooks/useMostRecentApiAuth";
+import { useNavigate } from "react-router-dom";
+import { appRoutes } from "config/router/paths";
+
+interface IProps extends IModalProps {}
+export const ExpiredSubscription: React.FC<IProps> = ({
+  open,
+  handleClose,
+}) => {
+  const navigate = useNavigate();
+
+  const handleConfirm = () => {
+    navigate(appRoutes.billingSubscription, { replace: true });
+  };
+  const { user, isLoading } = useMostRecentApiAuth();
+
+  return (
+    <ConfirmationModal
+      title="Expired Subscription"
+      handleClose={handleClose}
+      description={`Hello ${user?.fullName},
+      Your subscription has ended
+     Please do well to renew your subscription.
+     Thank you!`}
+      hideImage={true}
+      open={open}
+      loading={isLoading}
+      handleConfirm={{ fn: handleConfirm }}
+    />
+  );
+};
