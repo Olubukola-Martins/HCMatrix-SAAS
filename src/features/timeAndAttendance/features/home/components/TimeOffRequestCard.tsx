f3d459497556446513b26a8c569cdaa6151dd1de
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-03-20 16:02:16 +0100

    dashboard

--- src/features/timeAndAttendance/features/home/components/TimeOffRequestCard.tsx
+++ src/features/timeAndAttendance/features/home/components/TimeOffRequestCard.tsx
@@ -1,47 +1,91 @@
-import React from "react";
-// import { useApiAuth } from "hooks/useApiAuth";
 import { RecentCard } from "components/cards/RecentCard";
+import { usePagination } from "hooks/usePagination";
+import { useGetTimeOff } from "../../timeOff/hooks/useGetTimeOff";
+import { Dropdown, Empty, Menu, Skeleton } from "antd";
+import { Link } from "react-router-dom";
+import { appRoutes } from "config/router/paths";
 
-// TO DO: Remove this export and if need be move to styles/reused
-export const requestStyle =
-  "flex items-center justify-between cursor-pointer group border-b pb-2";
-
-export const LIMIT_OF_ITEMS_TO_DISPLAY = 3;
-
-export const TimeOffRequestCard: React.FC<{
-  handleSeeAll?: () => void;
-}> = ({ handleSeeAll }) => {
-//   const { token, companyId } = useApiAuth();
-
+export const TimeOffRequestCard = () => {
+  const { pagination } = usePagination({ pageSize: 2 });
+  const { data, isLoading } = useGetTimeOff({ pagination });
 
   return (
-    <RecentCard
-      title="Time Off Request"
-      total={3}
-    //   loading={isLoading}
-      data={[].map((item: any) => ({
-        title: `${item.employee.firstName} ${item.employee.lastName}`,
-        
-        features: [
-          {
-            name: "Status - ",
-            value: `${item.time}`,
-          },
-          {
-            name: "Time off policy - ",
-            value: `${item.date}`,
-          },
-          {
-            name: "Duration - ",
-            value: `${item.location}`,
-          },
-        ],
-        secondaryCol: {
-          type: "options",
-          options: [],
-        },
-      }))}
-      handleViewMore={handleSeeAll}
-    />
+    <div className="bg-mainBg pb-3 border rounded-lg text-sm shadow">
+      <div className="flex items-center justify-between px-3 py-3 border-b">
+        <p className="font-medium">Timesheet</p>
+        <span className="text-xs capitalize"></span>
+      </div>
+      <div className="flex flex-col gap-3 px-3 py-2">
+        <Skeleton loading={isLoading} active paragraph={{ rows: 4 }}>
+          {data?.data && data.data.length > 0 ? (
+            data?.data.map((item, i) => (
+              <div
+                className="flex items-center justify-between group border-b pb-2"
+                key={i}
+              >
+                <div className="flex flex-col gap-1">
+                  <h5 className="font-medium capitalize">
+                    {item.employee?.firstName} {item.employee?.lastName}
+                  </h5>
+
+                  <div className="flex flex-col gap-1">
+                    <span className="text-xs capitalize">
+                      Status: - {item.status}
+                    </span>
+                    <span className="text-xs capitalize">
+                      Time off Policy: - {item.policy?.title}
+                    </span>
+                    <span className="text-xs capitalize">
+                      Duration: -
+                      {`${item.policy?.duration} ${
+                        item.policy?.duration !== undefined
+                          ? item.policy.duration > 1
+                            ? "days"
+                            : "day"
+                          : "Unknown"
+                      }`}
+                    </span>
+                  </div>
+                </div>
+                <Dropdown
+                  trigger={["click"]}
+                  overlay={
+                    <Menu>
+                      <Menu.Item key={1}>
+                        {/* <Link
+                          to={
+                            appRoutes.timeSheetDetails(
+                              item?.employee?.id,
+                              item.clockIn.date
+                            ).path
+                          }
+                        >
+                          View details
+                        </Link> */}
+                      </Menu.Item>
+                    </Menu>
+                  }
+                >
+                  <i className="ri-more-fill text-base cursor-pointer" />
+                </Dropdown>
+              </div>
+            ))
+          ) : (
+            <>
+              <Empty />
+            </>
+          )}
+        </Skeleton>
+      </div>
+
+      <div className="flex justify-end mt-2">
+        <Link
+          to={appRoutes.timeOff}
+          className="text-caramel text-right px-3 text-sm font-semibold cursor-pointer hover:text-accent pb-2 pt-1"
+        >
+          See All
+        </Link>
+      </div>
+    </div>
   );
 };
--- src/features/timeAndAttendance/features/home/components/TimesheetCard.tsx
+++ src/features/timeAndAttendance/features/home/components/TimesheetCard.tsx
@@ -1,44 +1,80 @@
-import React from "react";
-import { RecentCard } from "components/cards/RecentCard";
 import { useGetTimeSheetRecord } from "../hooks/useGetTimeSheetRecord";
 import { usePagination } from "hooks/usePagination";
+import { Dropdown, Empty, Menu, Skeleton } from "antd";
+import { Link } from "react-router-dom";
+import { appRoutes } from "config/router/paths";
 
-export const LIMIT_OF_ITEMS_TO_DISPLAY = 3;
-
-export const TimesheetCard: React.FC<{
-  handleSeeAll?: () => void;
-}> = ({ handleSeeAll }) => {
+export const TimesheetCard = () => {
   const { pagination } = usePagination({ pageSize: 4 });
   const { data, isLoading } = useGetTimeSheetRecord({ pagination });
 
   return (
-    <RecentCard
-      title="Timesheet"
-      total={3}
-      loading={isLoading}
-      data={data?.data.map((item: any) => ({
-        title: `${item?.employee?.firstName} ${item?.employee?.lastName}`,
+    <div className="bg-mainBg pb-3 border rounded-lg text-sm shadow">
+      <div className="flex items-center justify-between px-3 py-3 border-b">
+        <p className="font-medium">Timesheet</p>
+        <span className="text-xs capitalize"></span>
+      </div>
+      <div className="flex flex-col gap-3 px-3 py-2">
+        <Skeleton loading={isLoading} active paragraph={{ rows: 4 }}>
+          {data?.data && data.data.length > 0 ? (
+            data?.data.map((item, i) => (
+              <div
+                className="flex items-center justify-between group border-b pb-2"
+                key={i}
+              >
+                <div className="flex flex-col gap-1">
+                  <h5 className="font-medium capitalize">
+                    {item.employee.firstName} {item.employee.lastName}
+                  </h5>
 
-        features: [
-          {
-            name: "Clocked in Time - ",
-            value: `${item.clockIn.time}`,
-          },
-          {
-            name: "Date:",
-            value: `${item.date}`,
-          },
-          // {
-          //   name: "Location:",
-          //   value: `${item.location}`,
-          // },
-        ],
-        secondaryCol: {
-          type: "options",
-          options: [],
-        },
-      }))}
-      handleViewMore={handleSeeAll}
-    />
+                  <div className="flex flex-col gap-1">
+                    <span className="text-xs capitalize">
+                      Clock In Time: - {item.clockIn.time}
+                    </span>
+                    <span className="text-xs capitalize">
+                      Date: - {item.clockIn.date}
+                    </span>
+                  </div>
+                </div>
+                <Dropdown
+                  trigger={["click"]}
+                  overlay={
+                    <Menu>
+                      <Menu.Item key={1}>
+                        <Link
+                          to={
+                            appRoutes.timeSheetDetails(
+                              item?.employee?.id,
+                              item.clockIn.date
+                            ).path
+                          }
+                        >
+                          View details
+                        </Link>
+                      </Menu.Item>
+                    </Menu>
+                  }
+                >
+                  <i className="ri-more-fill text-base cursor-pointer" />
+                </Dropdown>
+              </div>
+            ))
+          ) : (
+            <>
+              <Empty />
+            </>
+          )}
+        </Skeleton>
+      </div>
+
+      <div className="flex justify-end mt-2">
+        <Link
+          to={appRoutes.timeSheet}
+          className="text-caramel text-right px-3 text-sm font-semibold cursor-pointer hover:text-accent pb-2 pt-1"
+        >
+          See All
+        </Link>
+      </div>
+    </div>
   );
 };
