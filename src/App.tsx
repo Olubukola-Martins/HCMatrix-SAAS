6cb6f72078df5aeefc1b8dc80a8db0827f21f43f
kaasikodes <thisbroisfresh@gmail.com>
2024-04-22 10:32:21 +0100

    feat: ensures that app indicates when there in an internet connection issue

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Router from "config/router";
 import GlobalContextProvider from "stateManagers/GlobalContextProvider";
 import ErrorBoundary from "components/errorHandlers/ErrorBoundary";
 import { LOCAL_STORAGE_AUTH_KEY } from "constants/localStorageKeys";
+import { useNetworkState } from "hooks/network/useNetworkState";
+import { ErrorWrapper } from "components/errorHandlers/ErrorWrapper";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -28,26 +30,37 @@ function App() {
     }
     // localStorage.clear(); //to clear all changes tommorow
   }, []);
+
+  // check online status
+  const { isOnline } = useNetworkState();
   return (
     <ErrorBoundary message="Please contact administrator!">
-      <BrowserRouter>
-        <AuthProvider
-          authType={"localstorage"}
-          authName={LOCAL_STORAGE_AUTH_KEY}
-          // cookieDomain={window.location.hostname}
-          // cookieSecure={window.location.protocol === "https:"}
-          // refresh={refreshApi}
-        >
-          <QueryClientProvider client={queryClient}>
-            <GlobalContextProvider>
-              <Suspense fallback={<div>temporary Loading...</div>}>
-                <Router />
-              </Suspense>
-            </GlobalContextProvider>
-            <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-          </QueryClientProvider>
-        </AuthProvider>
-      </BrowserRouter>
+      <ErrorWrapper
+        isError={!isOnline}
+        message="Please check your internet connection!"
+      >
+        <BrowserRouter>
+          <AuthProvider
+            authType={"localstorage"}
+            authName={LOCAL_STORAGE_AUTH_KEY}
+            // cookieDomain={window.location.hostname}
+            // cookieSecure={window.location.protocol === "https:"}
+            // refresh={refreshApi}
+          >
+            <QueryClientProvider client={queryClient}>
+              <GlobalContextProvider>
+                <Suspense fallback={<div>temporary Loading...</div>}>
+                  <Router />
+                </Suspense>
+              </GlobalContextProvider>
+              <ReactQueryDevtools
+                initialIsOpen={false}
+                position="bottom-right"
+              />
+            </QueryClientProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      </ErrorWrapper>
     </ErrorBoundary>
   );
 }
