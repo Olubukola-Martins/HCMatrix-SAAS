6fc90b8a9176f730bac0967dfb43eb5728a831af
kaasikodes <thisbroisfresh@gmail.com>
2024-05-20 08:41:32 +0100

    chore: ensures that inactive sub middleware accounts 4 errors

--- src/components/middlewares/ActiveSubscriptionMiddleware.tsx
+++ src/components/middlewares/ActiveSubscriptionMiddleware.tsx
@@ -1,4 +1,5 @@
 import { Skeleton } from "antd";
+import { ErrorWrapper } from "components/errorHandlers/ErrorWrapper";
 import { appRoutes } from "config/router/paths";
 import { useGetCompanyActiveSubscription } from "features/billing/hooks/company/useGetCompanyActiveSubscription";
 import React from "react";
@@ -12,24 +13,34 @@ const ActiveSubscriptionMiddleware: React.FC<TProps> = ({
   children,
   isOwner,
 }) => {
-  const { data, isLoading } = useGetCompanyActiveSubscription();
+  const { data, isLoading, isError, error } = useGetCompanyActiveSubscription();
   const isCompanySubscriptionActive = !!data === true;
   return (
     <>
       <Skeleton active loading={isLoading} paragraph={{ rows: 10 }}>
-        {!isCompanySubscriptionActive && isOwner ? (
-          <Navigate
-            to={appRoutes.billingInactiveSubscriptionInformOwner}
-            replace={true}
-          />
-        ) : null}
-        {!isCompanySubscriptionActive && !isOwner ? (
-          <Navigate
-            to={appRoutes.billingInactiveSubscriptionInformEmployee}
-            replace={true}
-          />
-        ) : null}
-        {isCompanySubscriptionActive ? <>{children}</> : null}
+        <ErrorWrapper
+          isError={isError}
+          backLink={appRoutes.home}
+          message={
+            error?.response.data.message ?? error?.response.data.error.message
+          }
+        >
+          <>
+            {!isCompanySubscriptionActive && isOwner ? (
+              <Navigate
+                to={appRoutes.billingInactiveSubscriptionInformOwner}
+                replace={true}
+              />
+            ) : null}
+            {!isCompanySubscriptionActive && !isOwner ? (
+              <Navigate
+                to={appRoutes.billingInactiveSubscriptionInformEmployee}
+                replace={true}
+              />
+            ) : null}
+            {isCompanySubscriptionActive ? <>{children}</> : null}
+          </>
+        </ErrorWrapper>
       </Skeleton>
     </>
   );
