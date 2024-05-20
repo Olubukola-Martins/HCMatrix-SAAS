3a3358dff9232bf2f1073f6312765cbb2b46f31b
kaasikodes <thisbroisfresh@gmail.com>
2024-05-20 08:51:48 +0100

    chore: ensures permission data states are accounted 4 in sidebar links

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,36 +1,39 @@
 import { NavLink, useLocation } from "react-router-dom";
 import "./style/style.css";
 import { useGenerateDBSidebarLinks } from "hooks/dashboard/useGenerateDBSidebarLinks";
+import { Skeleton } from "antd";
 
 const SideBar = () => {
   const { pathname } = useLocation();
-  const { sidebarRoutes } = useGenerateDBSidebarLinks();
+  const { sidebarRoutes, isLoading } = useGenerateDBSidebarLinks();
   const isActiveRoute = (matcherKeys?: string[]) => {
     return matcherKeys?.some(
       (item) => pathname.toLowerCase().indexOf(item.toLowerCase()) !== -1
     );
   };
   return (
-    <div className="h-screen overflow-y-auto flex-col bg-card flex items-center px-2 text-center pb-32 scrollBar">
-      {sidebarRoutes
-        .filter((item) => item.hidden === false)
-        .map((route) => {
-          const isActive = isActiveRoute(route.matcherKeys);
+    <Skeleton loading={isLoading} paragraph={{ rows: 7 }}>
+      <div className="h-screen overflow-y-auto flex-col bg-card flex items-center px-2 text-center pb-32 scrollBar">
+        {sidebarRoutes
+          .filter((item) => item.hidden === false)
+          .map((route) => {
+            const isActive = isActiveRoute(route.matcherKeys);
 
-          return (
-            <NavLink
-              key={route.path}
-              to={route.path}
-              className={`sideBarItemWrap ${isActive ? "active" : ""}`}
-            >
-              <div className="flex justify-center">
-                <span className="sideBarList">{route.icon}</span>
-              </div>
-              <span className="sideBarName">{route.name}</span>
-            </NavLink>
-          );
-        })}
-    </div>
+            return (
+              <NavLink
+                key={route.path}
+                to={route.path}
+                className={`sideBarItemWrap ${isActive ? "active" : ""}`}
+              >
+                <div className="flex justify-center">
+                  <span className="sideBarList">{route.icon}</span>
+                </div>
+                <span className="sideBarName">{route.name}</span>
+              </NavLink>
+            );
+          })}
+      </div>
+    </Skeleton>
   );
 };
 
