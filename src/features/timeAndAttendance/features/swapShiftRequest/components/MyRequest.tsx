43cde1921e32a06c1ca01314e1331c67d5dc1c03
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-06-02 16:04:01 +0100

    swap design design

--- src/features/timeAndAttendance/features/swapShiftRequest/components/MyRequest.tsx
+++ src/features/timeAndAttendance/features/swapShiftRequest/components/MyRequest.tsx
@@ -1,17 +1,64 @@
 import { ColumnsType } from "antd/lib/table";
 import { allSwapRequestProps } from "../types";
 import { TableWithFocusType } from "components/table";
+import { Dropdown, Menu, Select } from "antd";
 
 export const MyRequest = () => {
   const columns: ColumnsType<allSwapRequestProps> = [
     {
-      title: "Name",
-      key: "name",
+      title: "Date",
+      key: "date",
+    },
+    {
+      title: "Default Shift",
+      key: "defaultShift",
+    },
+    {
+      title: "New Shift",
+      key: "newShift",
+    },
+    {
+      title: "Swap partner",
+      key: "swapPartner",
+    },
+    {
+      title: "Status",
+      key: "status",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, val) => (
+        <div>
+          <Dropdown
+            trigger={["click"]}
+            overlay={
+              <Menu>
+                <Menu.Item key="1">Edit</Menu.Item>
+                <Menu.Item key="2">Delete</Menu.Item>
+                <Menu.Item key="5">Cancel</Menu.Item>
+              </Menu>
+            }
+          >
+            <i className="ri-more-2-fill text-lg cursor-pointer"></i>
+          </Dropdown>
+        </div>
+      ),
     },
   ];
   return (
     <div className="mt-5">
-        
+      <Select
+        options={[
+          { value: "pending", label: "Pending" },
+          { value: "approved", label: "Approved" },
+          { value: "rejected", label: "Rejected" },
+          { value: "canceled", label: "Canceled" },
+        ]}
+        className="w-[7.8rem]"
+        placeholder="Status"
+        allowClear
+      />
       <TableWithFocusType columns={columns} dataSource={[]} />
     </div>
   );
--- src/features/timeAndAttendance/features/swapShiftRequest/components/MySwapApprovals.tsx
+++ src/features/timeAndAttendance/features/swapShiftRequest/components/MySwapApprovals.tsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import { allSwapRequestProps } from "../types";
+import { ColumnsType } from "antd/lib/table";
+import { Dropdown, Menu, Select } from "antd";
+import { TableWithFocusType } from "components/table";
 
 export const MySwapApprovals = () => {
+  const columns: ColumnsType<allSwapRequestProps> = [
+    {
+      title: "Date",
+      key: "date",
+    },
+    {
+      title: "Name",
+      key: "employee",
+    },
+    {
+      title: "Department",
+      key: "department",
+    },
+    {
+      title: "Default Shift",
+      key: "defaultShift",
+    },
+    {
+      title: "New Shift",
+      key: "newShift",
+    },
+    {
+      title: "Swap partner",
+      key: "swapPartner",
+    },
+    {
+      title: "Status",
+      key: "status",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, val) => (
+        <div>
+          <Dropdown
+            trigger={["click"]}
+            overlay={
+              <Menu>
+                <Menu.Item key="3">Reject</Menu.Item>
+                <Menu.Item key="4">Approve</Menu.Item>
+              </Menu>
+            }
+          >
+            <i className="ri-more-2-fill text-lg cursor-pointer"></i>
+          </Dropdown>
+        </div>
+      ),
+    },
+  ];
   return (
-    <div>MySwapApprovals</div>
-  )
-}
+    <div className="mt-5">
+      <Select
+        options={[
+          { value: "pending", label: "Pending" },
+          { value: "approved", label: "Approved" },
+          { value: "rejected", label: "Rejected" },
+        ]}
+        className="w-[7.8rem]"
+        placeholder="Status"
+        allowClear
+      />
+      <TableWithFocusType columns={columns} dataSource={[]} />
+    </div>
+  );
+};
