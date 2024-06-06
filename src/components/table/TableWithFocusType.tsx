9d24a844be781967bf973f04fd4f22ff7a178886
kaasikodes <thisbroisfresh@gmail.com>
2024-06-06 20:05:07 +0100

    refactor(table-focus-type): ensures resuability across projects

--- src/components/table/TableWithFocusType.tsx
+++ src/components/table/TableWithFocusType.tsx
@@ -2,9 +2,10 @@ import { Table } from "antd";
 import React, { useState } from "react";
 import { TableFocusTypeBtn } from "./TableFocusTypeBtn";
 import { ColumnsType } from "antd/lib/table";
+import { TableProps } from "antd/es/table";
 
-export function TableWithFocusType<TEntity>(
-  props: (typeof Table)["defaultProps"] & {
+export function TableWithFocusType<TEntity extends object>(
+  props: TableProps<TEntity> & {
     defaultSelectedColumns?: ColumnsType<TEntity>;
   }
 ): JSX.Element {
