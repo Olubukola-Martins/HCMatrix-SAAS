49752e63a13139f941caf4d632e84e597486fda9
kaasikodes <thisbroisfresh@gmail.com>
2024-06-18 16:59:38 +0100

    chore: adds shift category table to its corresponding container

--- src/features/timeAndAttendance/features/settings/workSchedule/components/shiftCategory/ShiftCategoryContainer.tsx
+++ src/features/timeAndAttendance/features/settings/workSchedule/components/shiftCategory/ShiftCategoryContainer.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { AddShiftCategoryBtn } from "./AddShiftCategory";
+import { ShiftCategoryTable } from "./ShiftCategoryTable";
 
 const ShiftCategoryContainer = () => {
   return (
     <div className="space-y-6">
       <AddShiftCategoryBtn className="flex justify-end" />
+      <ShiftCategoryTable />
     </div>
   );
 };
