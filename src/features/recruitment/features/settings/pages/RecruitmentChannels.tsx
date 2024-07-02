fcb3d206dfa0a9e9655d28c957fe18cd5e958224
Greattie <91846971+Greattie@users.noreply.github.com>
2024-07-02 15:06:53 +0100

    feat(recruitment-channels): add recruitment channels settings

--- src/features/recruitment/features/settings/pages/RecruitmentChannels.tsx
+++ src/features/recruitment/features/settings/pages/RecruitmentChannels.tsx
@@ -1,47 +1,42 @@
-// import { RecruitmentSettingsIntro } from "features/recruitment/components/RecruitmentSettingsIntro";
-// import { RecruitmentSettingsSwitchForm } from "../components/RecruitmentSettingsSwitchForm";
+import { RecruitmentSettingsIntro } from "features/recruitment/components/RecruitmentSettingsIntro";
+import { RecruitmentSettingsSwitchForm } from "../components/RecruitmentSettingsSwitchForm";
 
-// const candidateSources = [
-//   {
-//     label: "LinkedIn (Coming Soon)",
-//     name: "linkedIn",
-//   },
-// ];
-
-// export const RecruitmentChannels = () => {
-//   const onChange = (checked: boolean) => {
-//     console.log(`switch to ${checked}`);
-//   };
-//   return (
-//     <div>
-//       <RecruitmentSettingsIntro
-//         title="Recruitment Channels"
-//         description="Integrate more platforms to the system."
-//       />
-
-//       <div className="Container mt-5">
-//         <div className="bg-white rounded md:p-5 p-3">
-//           <h2 className="pb-5 font-medium text-base">Sources</h2>
-//           <div className="bg-mainBg py-4 px-4 rounded">
-//             {candidateSources.map((item) => (
-//               <RecruitmentSettingsSwitchForm
-//                 label={item.label}
-//                 name={item.name}
-//                 onChange={onChange}
-//               />
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-
-
+const candidateSources = [
+  {
+    label: "LinkedIn (Coming Soon)",
+    name: "linkedIn",
+  },
+];
 
 export const RecruitmentChannels = () => {
+  const onChange = (checked: boolean) => {
+    console.log(`switch to ${checked}`);
+  };
   return (
-    <div>RecruitmentChannels</div>
-  )
-}
+    <div>
+      <RecruitmentSettingsIntro
+        title="Recruitment Channels"
+        description="Integrate more platforms to the system."
+      />
+
+      <div className="Container mt-5">
+        <div className="bg-white rounded md:p-5 p-3">
+          <h2 className="pb-5 font-medium text-base">Sources</h2>
+          <div className="bg-mainBg py-4 px-4 rounded">
+            {candidateSources.map((item) => (
+              <RecruitmentSettingsSwitchForm
+                label={item.label}
+                name={item.name}
+                onChange={onChange}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+
