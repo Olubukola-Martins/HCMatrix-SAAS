a22ceaf569722022f261b046a90c594781f53d09
Greattie <91846971+Greattie@users.noreply.github.com>
2024-07-05 20:53:48 +0100

    feat(recruitemt-request-document):add request document feature in other settings

--- src/features/recruitment/features/settings/components/OtherSettings/PortalLink.tsx
+++ src/features/recruitment/features/settings/components/OtherSettings/PortalLink.tsx
@@ -15,13 +15,13 @@ export const PortalLink = () => {
   };
   return (
     <div className="Container">
-      <h2 className="text-base p-2 my-1">User Portal Link</h2>
+      <h2 className="text-base p-2 my-1">User Portal URL</h2>
       <Input
         placeholder="https://"
         value={inputValue}
         onChange={handleInputChange}
       />
-      
+
       <button
         onClick={handleCopyClick}
         className="flex p-2 my-4 gap-2 items-center border border-[#686868] rounded"
