fc190bd31092565d70554e0a2f5c4f88719e1282
Greattie <91846971+Greattie@users.noreply.github.com>
2024-07-05 10:22:54 +0100

    style(recruitment-benefits): add recruitment benefit in other settings

---
+++ src/features/recruitment/features/settings/components/OtherSettings/constants/DefaultOtherSettings.ts
@@ -0,0 +1,23 @@
+export const benfitsSettings = [
+  {
+    label: "Dental Insurance",
+    name: "dental",
+  },
+  {
+    label: "Flexibility Schedulde",
+    name: "flexibility",
+  },
+  {
+    label: "Health Insurance",
+    name: "health",
+  },
+  {
+    label: "Paid Time Off",
+    name: "paidTimeOff",
+  },
+  {
+    label: "Vision Insurance",
+    name: "vision",
+  },
+  
+];
