fb2f2368e16b47cae7175f42a12d1a68229ab55e
Greattie <91846971+Greattie@users.noreply.github.com>
2024-07-05 19:06:27 +0100

    feat(recruitment-portal-link): add portal link feature in other settings

--- src/features/recruitment/features/settings/components/OtherSettings/PortalLink.tsx
+++ src/features/recruitment/features/settings/components/OtherSettings/PortalLink.tsx
@@ -1,7 +1,15 @@
-
+import { Input } from "antd";
+import { IoCopyOutline } from "react-icons/io5";
 
 export const PortalLink = () => {
   return (
-    <div>PortalLink</div>
-  )
-}
+    <div className="Container">
+      <h2 className="text-base p-2 my-1">User Portal Link</h2>
+      <Input placeholder="https://" />
+      <button className="flex p-2 my-4 gap-2 items-center border border-[#686868] rounded">
+        <IoCopyOutline />
+        Copy link
+      </button>
+    </div>
+  );
+};
