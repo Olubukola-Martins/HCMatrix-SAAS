4c70049afba588c3c4076fdbfc5752a3b755ff21
Greattie <91846971+Greattie@users.noreply.github.com>
2024-07-08 11:39:23 +0100

    chore(recruitment-portal-link): update input function

--- src/features/recruitment/features/settings/features/otherSettings/components/PortalLink.tsx
+++ src/features/recruitment/features/settings/features/otherSettings/components/PortalLink.tsx
@@ -8,11 +8,17 @@ export const PortalLink = () => {
   const handleInputChange = (e: any) => {
     setInputValue(e.target.value);
   };
+
   const handleCopyClick = () => {
+    if (inputValue.trim() === "") {
+      alert("Input field is empty!");
+      return;
+    }
     navigator.clipboard.writeText(inputValue).then(() => {
       alert("Link copied to clipboard!");
     });
   };
+
   return (
     <div className="Container">
       <h2 className="text-base p-2 my-1">User Portal URL</h2>
@@ -21,7 +27,6 @@ export const PortalLink = () => {
         value={inputValue}
         onChange={handleInputChange}
       />
-      
       <button
         onClick={handleCopyClick}
         className="flex p-2 my-4 gap-2 items-center border border-[#686868] rounded"
