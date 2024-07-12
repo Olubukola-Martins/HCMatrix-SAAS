846f46354fc14c110646f682e2370c5d28bf715f
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-07-12 11:10:23 +0100

    recfactored 2fa components

--- src/features/core/twoFactorAuth/pages/TwoFactorAuthentication.tsx
+++ src/features/core/twoFactorAuth/pages/TwoFactorAuthentication.tsx
@@ -11,6 +11,7 @@ import { useGetTwoFA } from "../hooks/useGetTwoFA";
 import { Skeleton } from "antd";
 import { VerifyTwoFA } from "../components/VerifyTwoFA";
 import { ListBackupCodes } from "../components/ListBackupCodes";
+import { TwoFactorNotEnabled } from "../components/TwoFactorNotEnabled";
 
 const TwoFactorAuthentication = () => {
   const { isLoading, data: checkOtpData, isError } = useGetTwoFA();
@@ -71,33 +72,7 @@ const TwoFactorAuthentication = () => {
                   <EnableTwoFA setAction={setAction} setImage={setImage} />
                 ) : checkOtpData?.isDisabled === false &&
                   checkOtpData?.isVerified === true ? (
-                  <div className="text-sm">
-                    <p>
-                      Your account is already protected with Two-Factor
-                      <br className="md:flex hidden" />
-                      Authentication (2FA). This extra layer of security helps
-                      <br className="md:flex hidden" /> keep your account safe
-                      from unauthorized access.
-                    </p>
-                    <p className="pt-3">
-                      If you lose access to your authentication device, use any
-                      of
-                      <br className="md:flex hidden" />
-                      your backup codes to login to your account.
-                    </p>
-
-                    <div className="flex items-center justify-between mt-7">
-                      <AppButton
-                        label="Generate Backup codes"
-                        variant="transparent"
-                        handleClick={() => setAction("g-backup-codes")}
-                      />
-                      <AppButton
-                        label="Disable 2FA"
-                        handleClick={() => setAction("disable-2fa")}
-                      />
-                    </div>
-                  </div>
+                 <TwoFactorNotEnabled setAction={setAction}/>
                 ) : null}
               </div>
             </div>
