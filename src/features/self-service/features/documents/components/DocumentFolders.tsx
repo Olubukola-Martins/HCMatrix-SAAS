c8389808db3d0470cc539213d5fe153c13946050
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-08-27 08:46:36 +0100

    folders

--- src/features/self-service/features/documents/components/DocumentFolders.tsx
+++ src/features/self-service/features/documents/components/DocumentFolders.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { folderCard } from "../assets";
 import { useGetFolders } from "../hooks/useGetFolders";
 import { usePagination } from "hooks/usePagination";
 import dayjs from "dayjs";
-import { Dropdown, Menu } from "antd";
+import { Dropdown, Empty, Menu, Skeleton } from "antd";
 import { DeleteFolder } from "./folders/DeleteFolder";
 import { TFolderListItem } from "../types";
 
@@ -31,47 +31,57 @@ export const DocumentFolders = () => {
         folder={request}
       />
 
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {data?.data.map((item) => (
-          <div className="relative">
-            <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-between px-6 py-8 bg-white bg-opacity-75">
-              <div className="flex items-center justify-between">
-                <h5 className="text-base">{item.name}</h5>
-                <Dropdown
-                  trigger={["click"]}
-                  overlay={
-                    <Menu>
-                      <Menu.Item key="1">Add File</Menu.Item>
-                      <Menu.Item key="2">View Files</Menu.Item>
-                      <Menu.Item key="3" onClick={() => handleDelete(item)}>
-                        Delete Folder
-                      </Menu.Item>
-                    </Menu>
-                  }
-                >
-                  <i className="ri-more-2-fill text-lg cursor-pointer"></i>
-                </Dropdown>
-              </div>
-              <div className="flex items-center justify-between">
-                <h5 className="text-sm">
-                  <span className="text-2xl font-bold pr-1">
-                    {item.files.length}
+      {isLoading ? (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {[1, 2, 3, 4].map((item) => (
+            <Skeleton active loading={isLoading} key={item} />
+          ))}
+        </div>
+      ) : data?.data && data?.data.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {data?.data.map((item) => (
+            <div className="relative">
+              <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-between px-6 py-8 bg-white bg-opacity-75">
+                <div className="flex items-center justify-between">
+                  <h5 className="text-base">{item.name}</h5>
+                  <Dropdown
+                    trigger={["click"]}
+                    overlay={
+                      <Menu>
+                        <Menu.Item key="1">Add File</Menu.Item>
+                        <Menu.Item key="2">View Files</Menu.Item>
+                        <Menu.Item key="3" onClick={() => handleDelete(item)}>
+                          Delete Folder
+                        </Menu.Item>
+                      </Menu>
+                    }
+                  >
+                    <i className="ri-more-2-fill text-lg cursor-pointer"></i>
+                  </Dropdown>
+                </div>
+                <div className="flex items-center justify-between">
+                  <h5 className="text-sm">
+                    <span className="text-2xl font-bold pr-1">
+                      {item.files.length}
+                    </span>
+                    {`${item.files.length > 1 ? "Files" : "File"}`}
+                  </h5>
+                  <span className="text-sm">
+                    {dayjs(item.createdAt).format("Do MMM YYYY")}
                   </span>
-                  {`${item.files.length > 1 ? "Files" : "File"}`}
-                </h5>
-                <span className="text-sm">
-                  {dayjs(item.createdAt).format("Do MMMM YYYY")}
-                </span>
+                </div>
               </div>
+              <img
+                src={folderCard}
+                alt="folder-background"
+                className="w-full object-cover"
+              />
             </div>
-            <img
-              src={folderCard}
-              alt="folder-background"
-              className="w-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      ) : (
+        <Empty className="mt-15" />
+      )}
     </div>
   );
 };
