c5abec885a1dbcdfefa509dc4947bee30d191c50
GodswillOmenuko <godswill.omenuko@tezzasolutions.com>
2024-09-04 09:21:56 +0100

    bug fixes

--- src/features/timeAndAttendance/features/home/components/home/AttendanceWelcomeHeader.tsx
+++ src/features/timeAndAttendance/features/home/components/home/AttendanceWelcomeHeader.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IDivProps } from "types/html";
 import FilterDBBtn, { TFilterAttendanceDBBtnProps } from "./FilterDBBtn";
+import { PermissionRestrictor } from "components/permission-restriction/PermissionRestrictor";
 
 type IProps = IDivProps & {
   greeting: string;
@@ -29,7 +30,11 @@ const AttendanceWelcomeHeader: React.FC<IProps> = ({
         <button className="border rounded cursor-text px-3 py-2 gap-x-3 font-medium">
           {formattedToday}
         </button>
+        <PermissionRestrictor
+          requiredPermissions={["view-all-time-and-attendance-timesheet"]}
+        >
         <FilterDBBtn {...filterProps} />
+        </PermissionRestrictor>
       </div>
     </div>
   );
