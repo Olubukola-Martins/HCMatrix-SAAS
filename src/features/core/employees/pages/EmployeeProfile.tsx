3b2a93cb23f60cfe49ab546c5295531b9f6e309f
Kaasi Kodes <thisbroisfresh@gmail.com>
2024-09-25 08:31:51 +0100

    chore: ensures that owners n sufficient permissions can update employee info , independent of employee

--- src/features/core/employees/pages/EmployeeProfile.tsx
+++ src/features/core/employees/pages/EmployeeProfile.tsx
@@ -1,15 +1,24 @@
 import { useParams } from "react-router-dom";
 import { EmployeeProfileContainer } from "../components/MyProfile/EmployeeProfileContainer";
-import useMostRecentApiAuth from "hooks/useMostRecentApiAuth";
+import {
+  canUserAccessComponent,
+  useGetUserPermissions,
+} from "components/permission-restriction/PermissionRestrictor";
 
 export const EmployeeProfile = () => {
   const params = useParams();
   const employeeId = params.id as string;
-  const { currentCompanyEmployeeDetails: user } = useMostRecentApiAuth();
+  const { userPermissions, isOwner } = useGetUserPermissions();
   return (
     <EmployeeProfileContainer
       employeeId={+employeeId}
-      isOwner={user?.isOwner}
+      isOwner={
+        isOwner ||
+        canUserAccessComponent({
+          userPermissions,
+          requiredPermissions: ["manage-employees"],
+        })
+      } //this is made optional, so that users more than user (isOwner) is capable of editing employee info directly, and those with the stated permission above can do so
     />
   );
 };
