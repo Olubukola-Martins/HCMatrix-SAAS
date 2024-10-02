0c7f485272d04e12b42a18415bbcdcc13d16796e
Olubukola-Martins <19bukola97@gmail.com>
2024-10-03 00:29:26 +0100

    refactor(recaptcha):recaptcha ui refactor

--- src/components/recaptcha/Recaptcha.tsx
+++ src/components/recaptcha/Recaptcha.tsx
@@ -1,13 +1,21 @@
 import { useEffect } from "react";
-import { loadCaptchaEnginge, LoadCanvasTemplate } from "react-simple-captcha";
+import { RxReload } from "react-icons/rx";
+import { loadCaptchaEnginge, LoadCanvasTemplateNoReload } from "react-simple-captcha";
 
 const Recaptcha = () => {
   useEffect(() => {
     loadCaptchaEnginge(6);
   }, []);
+
+  const reloadCaptcha = () => {
+    loadCaptchaEnginge(6);
+  };
   return (
-    <div>
-      <LoadCanvasTemplate />
+    <div className="flex justify-between py-3  align-middle">
+      <div className=" w-60 h-10 rounded-md border border-gray-400 flex justify-center py-1 ">
+        <LoadCanvasTemplateNoReload />
+      </div>
+      <RxReload onClick={reloadCaptcha} className="text-caramel border border-caramel rounded-md h-[39px] w-16 p-2 cursor-pointer" size={16}/>
     </div>
   );
 };
