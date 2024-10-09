3c12527829921887ba76880065ee394500b8e61e
Kaasi Kodes <thisbroisfresh@gmail.com>
2024-10-09 05:41:09 +0100

    chore: ensures the abilty to directly edit profile under predefined conditions

--- src/features/core/employees/pages/EmployeeProfile.tsx
+++ src/features/core/employees/pages/EmployeeProfile.tsx
@@ -9,16 +9,16 @@ export const EmployeeProfile = () => {
   const params = useParams();
   const employeeId = params.id as string;
   const { userPermissions, isOwner } = useGetUserPermissions();
+  const canEditProfile =
+    isOwner ||
+    canUserAccessComponent({
+      userPermissions,
+      requiredPermissions: ["manage-employees"],
+    });
   return (
     <EmployeeProfileContainer
       employeeId={+employeeId}
-      isOwner={
-        isOwner ||
-        canUserAccessComponent({
-          userPermissions,
-          requiredPermissions: ["manage-employees"],
-        })
-      } //this is made optional, so that users more than user (isOwner) is capable of editing employee info directly, and those with the stated permission above can do so
+      isOwner={canEditProfile} //this is made optional, so that users more than user (isOwner) is capable of editing employee info directly, and those with the stated permission above can do so
     />
   );
 };
--- src/features/core/employees/pages/MyProfile.tsx
+++ src/features/core/employees/pages/MyProfile.tsx
@@ -1,8 +1,24 @@
 import { useApiAuth } from "hooks/useApiAuth";
 import { EmployeeProfileContainer } from "../components/MyProfile/EmployeeProfileContainer";
+import {
+  canUserAccessComponent,
+  useGetUserPermissions,
+} from "components/permission-restriction/PermissionRestrictor";
 
 export const MyProfile = () => {
   const { currentUserEmployeeId } = useApiAuth();
+  const { userPermissions, isOwner } = useGetUserPermissions();
+  const canEditProfile =
+    isOwner ||
+    canUserAccessComponent({
+      userPermissions,
+      requiredPermissions: ["manage-employees"],
+    });
 
-  return <EmployeeProfileContainer employeeId={currentUserEmployeeId} />;
+  return (
+    <EmployeeProfileContainer
+      employeeId={currentUserEmployeeId}
+      isOwner={canEditProfile}
+    />
+  );
 };
